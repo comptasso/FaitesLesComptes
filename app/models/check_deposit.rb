19842f06fefc9c3eb4851f69da895ca5c22121b6
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2011-12-11 09:06:49 +0100

    la suppression d une remise de chèque remet le champ check_deposit_id à nil

--- app/models/check_deposit.rb
+++ app/models/check_deposit.rb
@@ -2,5 +2,14 @@ class CheckDeposit < ActiveRecord::Base
   has_many :lines
   belongs_to :bank_account
 
-  
+  before_destroy :remove_check_deposit_id_in_lines
+
+  private
+
+  def remove_check_deposit_id_in_lines
+    self.lines.each do |l|
+      l.update_attribute(:check_deposit_id, nil)
+      l.save
+    end
+  end
 end
