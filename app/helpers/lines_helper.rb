7e03a02f22f2018e3c9bccc57e51f5a3cc174a1b
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2011-12-14 12:45:04 +0100

    test du helper pour le sous menu

--- app/helpers/lines_helper.rb
+++ app/helpers/lines_helper.rb
@@ -4,7 +4,7 @@ module LinesHelper
     if montant > -0.01 && montant < 0.01
       '-'
     else
-      number_with_precision(montant, precision: 2)
+      number_with_precision(montant, :precision=> 2)
     end
   rescue
     ''
@@ -12,13 +12,23 @@ module LinesHelper
 
  
 
-  def submenu(book)
+  def submenu_helper(book)
     t=['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',' Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
-     content_tag :div, link_to('Janvier', book_lines_path(book, 'Janvier'=> 0))
-    content_tag :div do
-      t.each_with_index do |mois,i|
-         link_to(mois, book_lines_path(book, "#{mois}"=> i))
-      end
-    end
+#    content_tag(:div) do
+#      concat(link_to('Janvier', book_lines_path(book, 'mois'=> 0)))
+#       concat(link_to('Février', book_lines_path(book, 'mois'=> 1)))
+#    end
+#
+#
+
+content_tag :div do
+   t.sum { |mois| content_tag(:p, mois) }
+end
+#    a= content_tag :span do
+#      t.each_with_index do |mois,i|
+#         link_to(mois, book_lines_path(book, "#{mois}"=> i))
+#      end
+#    end
+#    a
   end
 end
