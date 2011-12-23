4b174dc038cb0d3dc972e8c69993ed5512510010
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2011-12-23 17:59:31 +0100

    méthode fill_bank_lines appelée par bank_extract#after_create pour préremplir un extrait de compte

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -7,11 +7,15 @@ class BankAccount < ActiveRecord::Base
 
   # Méthode qui donne le montant du dernier solde bancaire
   # par ordre de date
-  #
+  
   def last_bank_extract_sold
-    self.bank_extracts.order(:end_date).last.end_sold
+    self.last_bank_extract.sold
   rescue
     0
+  end
+
+  def last_bank_extract_debit_credit
+   return self.last_bank_extract.debit, self.last_bank_extract.credit
   end
 
   def last_bank_extract_day
@@ -19,20 +23,35 @@ class BankAccount < ActiveRecord::Base
   rescue
     Date.today.beginning_of_month - 1
   end
-
- def not_pointed_lines
-  ls=  Line.find_by_sql("SELECT id, narration, debit, credit, payment_mode, line_date
+ # trouve toutes les lignes non pointées -np pour not pointed
+ def np_lines
+   Line.find_by_sql("SELECT id, narration, debit, credit, payment_mode, line_date
     FROM LINES WHERE (BANK_ACCOUNT_ID = #{self.id} AND ((PAYMENT_MODE != 'Chèque') or (credit < 0.001))) AND NOT EXISTS (SELECT * FROM BANK_EXTRACT_LINES WHERE LINE_ID = LINES.ID)")
-    ls.map {|l| BankExtractLine.new(:line_id=>l.id)}
-
  end
 
+ # crée des bank_extract_lines à partir des lignes non pointées
+ def not_pointed_lines
+  self.np_lines.map {|l| BankExtractLine.new(:line_id=>l.id)}
+ end
+
+# Trouve toutes les remises de chèques qui ne sont pas encore pointées
+ def np_check_deposits
+   self.check_deposits.where('bank_extract_id IS NULL')
+ end
+
+ # Crée des bank_extract_lines à partir des check_deposits non pointés
  def not_pointed_check_deposits
-    self.check_deposits.where('bank_extract_id IS NULL').map {|cd| BankExtractLine.new(:check_deposit_id=>cd.id)}
+    self.np_check_deposits.map {|cd| BankExtractLine.new(:check_deposit_id=>cd.id)}
  end
 
  def lines_to_point
    self.not_pointed_lines +  self.not_pointed_check_deposits
  end
+
+ private
+
+ def last_bank_extract
+    self.bank_extracts.order(:end_date).last
+  end
 
 end
--- app/models/bank_extract.rb
+++ app/models/bank_extract.rb
@@ -7,20 +7,6 @@ class BankExtract < ActiveRecord::Base
   after_create :fill_bank_extract_lines
   after_save :lock_lines_if_locked
 
-
-
-#  def fill_bank_extract_lines
-#    # TRAITEMENT DES LIGNES
-#    # on recherche toutes les lignes qui ne sont pas déja rattachées à un relevé de compte
-#    Line.where('check_deposit_id IS NULL').where('lines.')
-#    # et qui ne relèvent pas non plus d'une remise de chèques
-#    # on associe toutes celles dont la date est antérieure à la clôture
-#
-#    # TRAITEMENT DES REMISES DE CHEQUES
-#    # on regarde toutes celles antérieures à la date de fin du relevé
-#    # et on les associe
-#  end
-
   def lockable?
     !self.locked? && self.equality?
   end
@@ -46,7 +32,7 @@ class BankExtract < ActiveRecord::Base
   end
 
   def equality?
-    (self.diff_debit.abs < 0.001) && (self.diff_credit < 0.001)
+    (self.diff_debit.abs < 0.001) && (self.diff_credit.abs < 0.001)
   end
 
   def lines_sold
@@ -59,8 +45,17 @@ class BankExtract < ActiveRecord::Base
 
   private
 
+  # méthode appelée après la création d'un bank_extract
+  # tente de pré remplir les lignes du relevé bancaire 
+  # prend l'ensemble des lignes non pointées et 
+  # crée des bank_extract_lines pour toutes les lignes dont les dates sont inférieures à la date de clôture
   def fill_bank_extract_lines
-    # TODO fill_bank_extract_lines
+    npl=self.bank_account.np_lines
+    npl.reject! {|l| l.line_date > self.end_date}
+    npl.each {|l| BankExtractLine.create!(bank_extract_id: self.id, line_id: l.id)}
+    cdl=self.bank_account.np_check_deposits
+    cdl.reject! {|l| l.deposit_date > self.end_date}
+    cdl.each {|l| BankExtractLine.create!(bank_extract_id: self.id, check_deposit_id: l.id)}
   end
 
   def lock_lines_if_locked
