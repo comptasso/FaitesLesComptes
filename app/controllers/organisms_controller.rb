b2207ab15292606ed46307eb1202a4cdccffa821
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2011-12-29 08:03:23 +0100

    supprime les actions transférées dans la zone admin du controller organism

--- app/controllers/organisms_controller.rb
+++ app/controllers/organisms_controller.rb
@@ -31,65 +31,7 @@ class OrganismsController < ApplicationController
     end
   end
 
-  # GET /organisms/new
-  # GET /organisms/new.json
-  def new
-    @organism = Organism.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @organism }
-    end
-  end
-
-  # GET /organisms/1/edit
-  def edit
-    @organism = Organism.find(params[:id])
-  end
-
-  # POST /organisms
-  # POST /organisms.json
-  def create
-    @organism = Organism.new(params[:organism])
-
-    respond_to do |format|
-      if @organism.save
-        format.html { redirect_to @organism, notice: "Création de l'organisme effectuée" }
-        format.json { render json: @organism, status: :created, location: @organism }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @organism.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /organisms/1
-  # PUT /organisms/1.json
-  def update
-    @organism = Organism.find(params[:id])
-
-    respond_to do |format|
-      if @organism.update_attributes(params[:organism])
-        format.html { redirect_to @organism, notice: "Modification de l'organisme effectuée" }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @organism.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /organisms/1
-  # DELETE /organisms/1.json
-  def destroy
-    @organism = Organism.find(params[:id])
-    @organism.destroy
-
-    respond_to do |format|
-      format.html { redirect_to organisms_url }
-      format.json { head :ok }
-    end
-  end
+ 
 
   def stats
     @date_from = params[:pick_date_from] ? picker_to_date(params[:pick_date_from]) : Date.today.beginning_of_year
