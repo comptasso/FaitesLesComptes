37b41a25e901487829eff7f427072517a8fc8955
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-02-20 18:08:10 +0100

    création du partial graphic

--- app/helpers/organisms_helper.rb
+++ app/helpers/organisms_helper.rb
@@ -2,15 +2,41 @@ module OrganismsHelper
   
   # Forunit à la vue les informations cachées pour construire le graphe
   #
-  def result_datas
-    content_tag :div, :class=>"datas_for_graph", :id=>"datas_result" do #, style="display: none;"> do
-        content_tag :span, :id=>"series_result" do
-           @graph_result.legend.join(';')
-        end
-    end
-#  <span id#="months_list_<%= book.id %>">  <%= book.graphic.ticks.join(';') %></span>
-#  <span id#="datas_list_<%= book.id %>">  <%= book.graphic.series[1].join(';')  %></span>
-#  <span id#="previous_datas_list_<%= book.id %>">  <%= book.graphic.series[0].join(';') %></span>
- #</div>
-  end
-end
+#  def result_datas
+#    content_tag :div, :class=>"datas_for_graph", :id=>"datas_result" do #, style="display: none;"> do
+#      content_tag :span, :id=>"series_result" do
+#        @graph_result.legend.join(';')
+#      end
+#    end
+#    #  <span id#="months_list_<%= book.id %>">  <%= book.graphic.ticks.join(';') %></span>
+#    #  <span id#="datas_list_<%= book.id %>">  <%= book.graphic.series[1].join(';')  %></span>
+#    #  <span id#="previous_datas_list_<%= book.id %>">  <%= book.graphic.series[0].join(';') %></span>
+#    #</div>
+#  end
+#
+#  def pave_with_graph(title, classe=nil)
+#
+#    content_tag :div, :class=>"pave #{classe}" do
+#      content_tag :h2 do
+#        title
+#      end
+#      content_tag :div, class: "monthly_graphic", id: "monthly_graphic_book_0" do
+#          content_tag :div, class: "datas_for_graph", id: "datas_0" do
+#             content_tag :span,  id: "series_0" do
+#                @graph_result.legend.join(';')
+#              end
+#              content_tag :span, id: "months_list_0" do
+#                @graph_result.ticks.join(';')
+#              end
+#              content_tag :span, id: "datas_list_0" do
+#                @graph_result.series[1].join(';')
+#              end
+#              content_tag :span, id: "previous_datas_list_0" do
+#                @graph_result.series[0].join(';')
+#              end
+#            end
+#          end
+#          content_tag :div, class: "bar_graph", id: "chart_0"
+#       end
+#      end
+   end
