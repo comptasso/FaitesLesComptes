4fabea55d325df684585fc4bc7dcf72c13ff7061
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-03-02 14:56:22 +0100

    fin de la refonte de check_deposit avec l introduction d un proc pour la condition has_many checks

--- app/models/check_deposit.rb
+++ app/models/check_deposit.rb
@@ -11,16 +11,13 @@
 # la champ bank_account_id des lignes est rempli avec le bank_account_id du check_deposit
 class CheckDeposit < ActiveRecord::Base
 
-  # La condition est mise ici pour éviter que check_deposit.new soit associée d'emblée
-  # à toutes les lignes qui n'ont pas de check_depsoit_id
-  # L'objectif est que check_deposit soit capable de donner la somme des chèques en partant de zero
+  # La condition est mise ici pour que check_deposit.new soit associée d'emblée
+  # à toutes les lignes qui correspondant aux chèques en attente d'encaissement
+  # de l'organisme correspondant.
   belongs_to :bank_account
   has_many :checks, class_name: 'Line',
                     dependent: :nullify,
-# FIXME ici il ne devrait y avoir que les chèques relevant de l'organisme concerné
-# pour éviter le mélande des chèques de différents organismes.
-
-                    conditions: 'credit > 0 and payment_mode = "Chèque" ', # and book_id IN ("#{bids}")',
+                    conditions: Proc.new {"credit > 0 and payment_mode='Chèque' and book_id IN (#{self.bids}) "},
                     before_remove: :cant_if_pointed, #on ne peut retirer un chèque si la remise de chèque a été pointée avec le compte bancaire
                     after_remove: :nil_bank_account_id,
                     before_add: :cant_if_pointed  do
@@ -31,10 +28,12 @@ class CheckDeposit < ActiveRecord::Base
                   def total
                     proxy_association.target.inject(0) {|i,l| i += l.credit}
                   end
-
-               
                end
 
+  def bids
+    raise "Modèle CheckDeposit - Impossible de trouver les livres sans avoir l'organisme" if self.bank_account_id == nil
+    bank_account.organism.income_books.all.collect {|b| b.id}.join(',')
+  end
 
   # c'est la présence de bank_extract_line qui indique que le check_deposit à été pointé et ne peut plus être modifié
   has_one :bank_extract_line
@@ -42,7 +41,6 @@ class CheckDeposit < ActiveRecord::Base
   validates :bank_account, :deposit_date, :presence=>true
   validates :deposit_date,:bank_account_id, :cant_change=>true  if :bank_extract_line
  
-
   before_validation :not_empty # une remise chèque vide n'a pas de sens
 
   after_save :update_checks if :bank_extract_line
@@ -50,19 +48,24 @@ class CheckDeposit < ActiveRecord::Base
 
   before_destroy :cant_destroy_when_pointed
 
+  # permet de trouver les check à encaisser
   def self.pending_checks(organism)
     organism.pending_checks
   end
-  
- 
+
+  # donne le total des chèques à encaisser pour cet organisme
   def self.total_to_pick(organism)
     pending_checks(organism).sum(:credit)
   end
 
+  # donne le nombre total des chèques à encaisser pour un organisme
   def self.nb_to_pick(organism)
     pending_checks(organism).count
   end
 
+  # pour remplir la remise de chèques avec la totalité des chèques disponibles
+  # l'organisme peut être fourni en paramètre ou être déduit de self si bank_account est
+  # informé
   def pick_all_checks(org =nil)
     org ||= bank_account.organism
     raise 'Undefined organism' unless org
@@ -71,6 +74,7 @@ class CheckDeposit < ActiveRecord::Base
 
   private
 
+ # appelé par before_save pour éviter les remises chèques vides
   def not_empty
        self.errors[:base] <<  'Il doit y avoir au moins un chèque dans une remise' if checks.empty?
   end
@@ -87,6 +91,7 @@ class CheckDeposit < ActiveRecord::Base
   end
 
   
+  # appelé par before_destroy pour interdire la destruction d'une remise de chèque pointée
   def cant_destroy_when_pointed
     if bank_extract_line
     logger.warn "Tentative de détruire la remise de chèques #{id}, alors qu'elle est pointée sur une ligne de comte #{bank_extract_line.id}"
@@ -103,11 +108,13 @@ class CheckDeposit < ActiveRecord::Base
      
   end
 
+  # met à jour les lignes avec le champ bank_account_id
   def update_checks_with_bank_account_id
     checks.each {|l| l.update_attribute(:bank_account_id, bank_account.id)}
   end
 
 
+  # remet à nil le champ bank_account_id des lignes lorsqu'on les retire de la remise de chèques
   def nil_bank_account_id(line)
     line.update_attribute(:bank_account_id, nil)
   end
