c4ff7cb31d932af4650b2d19e40f0532454f6a83
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-03-05 09:54:06 +0100

    commenté la classe Utilities::Graphic et rebaptisé readme en roadmap

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,14 +11,18 @@ class Book < ActiveRecord::Base
 
   # les chèques en attente de remise en banque
   has_many :pending_checks,
-     :class_name=>'Line',
-     :conditions=>'payment_mode = "Chèque" and credit > 0 and check_deposit_id IS NULL'
+    :class_name=>'Line',
+    :conditions=>'payment_mode = "Chèque" and credit > 0 and check_deposit_id IS NULL'
    
   validates :title, presence: true 
  
   attr_reader :graphic, :monthly_solds
 
 
+
+  # cette partie GRAPHIQUE permet de construire un graphique à partir des données du livre
+
+  # permet de retourner ou de créer la variable d'instance
   def graphic(period=nil)
     @graphic ||= default_graphic(period)
   end
@@ -35,12 +39,15 @@ class Book < ActiveRecord::Base
     end
   end
 
+
+  # la construction d'un graphique sur un an
   def one_year_monthly_graphic(period)
     mg= Utilities::Graphic.new(self.ticks(period))
     mg.add_serie(:legend=>period.exercice, :datas=>self.monthly_datas_for_chart(period.list_months('%m-%Y')), :period_id=>period.id )
     mg
   end
 
+  # construction d'un graphique sur deux ans
   def two_years_monthly_graphic(period)
     mg= Utilities::Graphic.new(self.ticks(period))
     months= period.list_months('%m-%Y') # les mois du dernier exercice servent de référence
@@ -62,14 +69,12 @@ class Book < ActiveRecord::Base
 
   # renvoie le solde d'un livre pour un mois donné au format mm-yyyy
   def monthly_sold(month)
-    
     ls=self.lines.month(month)
     ls.sum(:credit)-ls.sum(:debit)
-
   end
 
 
- #  protected
+  #  protected
 
   def ticks(period)
     period.list_months('%b')
@@ -78,24 +83,23 @@ class Book < ActiveRecord::Base
   
   # calcule le total des lignes pour chacun des mois de l'exercice transmis en paramètres
   # renvoie un array donnant le total credit - debit des lignes des mois
-  # On utilise les mois de la période (var months) ou ceux fournis par la variable months
-  # pour pouvoir avoir des exercices de longueur différente.
-  #
-  # FIXME il y aura un problème avec les exercices de plus de 12 mois. - A réfléchir
-  #
+  # En pratique, on appelle les deux méthodes avec la même variable months
+  # pour avoir des séries de même longueur même lorsque les éxercices sont de longueur différentes.
+  # month doit être au format mm-yyyy
+  # 
   def monthly_datas_for_chart(months)
-     months.map {|m| self.monthly_sold(m)}
- end
+    months.map {|m| self.monthly_sold(m)}
+  end
 
- def previous_year_monthly_datas_for_chart(months)
-   a=[]
-   months.each do |m|
-       month= m[/^\d{2}/]
-       year = m[/\d{4}$/].to_i - 1
-   a << self.monthly_sold("#{month}-#{year}")
+  def previous_year_monthly_datas_for_chart(months)
+    a=[]
+    months.each do |m|
+      month= m[/^\d{2}/]
+      year = m[/\d{4}$/].to_i - 1
+      a << self.monthly_sold("#{month}-#{year}")
     end
     a
- end
+  end
 
   
 
--- app/models/utilities/graphic.rb
+++ app/models/utilities/graphic.rb
@@ -8,10 +8,17 @@
 # puis en ajoutant (add_serie) des séries sous forme de hash avec légend et datas
 # Des erreurs sont levées s'il manque la légende, ou si le nombre de datas est différents de ticks
 #
+# Chaque série ajoutée est un hash dormé de :legend et :datas. Une information additionnelle
+# peut être fournie avec la clé :period_id. Cette information permet de cliquer sur
+# un élément du graphe et d'aboutir à la page correspondante (essentiel lorsque le graphe
+# comprend plusieurs exercices)
+#
   class  Utilities::Graphic
 
     attr_reader :ticks, :series, :legend, :period_ids
 
+    # initialisation avec l'axe des x sous forme d'array.
+    # le contenu de l'array sera les étiquettes de l'axe des abscisses
     def initialize(ticks)
       raise 'Ticks should be an array with at least one element' unless ticks.is_a?(Array) && !ticks.empty?
        @ticks=ticks
@@ -19,6 +26,7 @@
        @legend=[]
        @period_ids=[]
     end
+
 
     def add_serie(serie)
       check_serie(serie)
@@ -32,7 +40,10 @@
       @series.size
     end
 
+    
+    # surcharge de l'opérateur égalité
     def ==(graph)
+
     return false if self.legend != graph.legend
     return false if self.nb_series != graph.nb_series
     self.series.each_with_index do |s,i|
@@ -46,6 +57,8 @@
 
     protected
 
+    # vérifie que les infos indispensables sont là
+    # et que la taille de la série est cohérente avec les ticks
     def check_serie(serie)
       raise 'Missing datas for this serie' if serie[:datas].nil?
       raise 'Missing legend for this serie' if serie[:legend].nil?
