6a73484521e2dd9333d2a212be10bf747b747822
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-03-07 08:11:21 +0100

    simplifier le cash_submenu_helper

--- app/helpers/cash_lines_helper.rb
+++ app/helpers/cash_lines_helper.rb
@@ -2,25 +2,12 @@
 module CashLinesHelper
 
   def cashsubmenu_helper(cash, period)
-   t=[]
-   if period
-     t= period.list_months('%b')
-   else
-     t=['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',' Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
-   end
- content_tag :span do
-     s=''
-     t.each_with_index do |mois, i|
-
-        u =  content_tag :span do
-             link_to_unless_current(mois, cash_cash_lines_path(cash, "mois"=> i))
-        end
-        s += concat(u)
+    content_tag :span do
+      s=''
+      period.list_months('%b').each_with_index do |mois, i|
+        s +=concat(  content_tag(:span) { link_to_unless_current(mois, cash_cash_lines_path(cash, "mois"=> i)) } )
+      end
+      s
     end
-    s
   end
-
-
-   
-end
 end
