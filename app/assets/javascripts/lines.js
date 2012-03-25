1addb5fc8aa51847a459ea584ebb39f176ec5abe
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-03-25 22:48:54 +0200

    repris l affichage de lines avec bootstrap - en cours

--- app/assets/javascripts/lines.js
+++ app/assets/javascripts/lines.js
@@ -10,6 +10,105 @@ jQuery(function (){
 });
 
 
+/* API method to get paging information */
+$.fn.dataTableExt.oApi.fnPagingInfo = function (oSettings) {
+    return {
+        "iStart":         oSettings._iDisplayStart,
+        "iEnd":           oSettings.fnDisplayEnd(),
+        "iLength":        oSettings._iDisplayLength,
+        "iTotal":         oSettings.fnRecordsTotal(),
+        "iFilteredTotal": oSettings.fnRecordsDisplay(),
+        "iPage":          Math.ceil(oSettings._iDisplayStart / oSettings._iDisplayLength),
+        "iTotalPages":    Math.ceil(oSettings.fnRecordsDisplay() / oSettings._iDisplayLength)
+    };
+};
+
+
+
+/* Bootstrap style pagination control */
+$.extend($.fn.dataTableExt.oPagination, {
+    "bootstrap": {
+        "fnInit": function (oSettings, nPaging, fnDraw) {
+            var oLang, fnClickHandler, els;
+            oLang = oSettings.oLanguage.oPaginate;
+            fnClickHandler = function (e) {
+                e.preventDefault();
+                if (oSettings.oApi._fnPageChange(oSettings, e.data.action)) {
+                    fnDraw(oSettings);
+                }
+            };
+
+            $(nPaging).addClass('pagination').append(
+                '<ul>' +
+                    '<li class="prev disabled"><a href="#">&larr; ' + oLang.sPrevious + '</a></li>' +
+                    '<li class="next disabled"><a href="#">' + oLang.sNext + ' &rarr; </a></li>' +
+                    '</ul>'
+            );
+            els = $('a', nPaging);
+            $(els[0]).bind('click.DT', { action: "previous" }, fnClickHandler);
+            $(els[1]).bind('click.DT', { action: "next" }, fnClickHandler);
+        },
+
+        "fnUpdate": function (oSettings, fnDraw) {
+            var iListLength = 5,
+                iLen = 0,
+                oPaging = oSettings.oInstance.fnPagingInfo(),
+                an = oSettings.aanFeatures.p,
+                i,
+                j,
+                sClass,
+                iStart,
+                iEnd,
+                iHalf = Math.floor(iListLength / 2),
+                jcf = function (e) {
+                    e.preventDefault();
+                    oSettings._iDisplayStart = (parseInt($('a', this).text(), 10) - 1) * oPaging.iLength;
+                    fnDraw(oSettings);
+                };
+
+            if (oPaging.iTotalPages < iListLength) {
+                iStart = 1;
+                iEnd = oPaging.iTotalPages;
+            } else if (oPaging.iPage <= iHalf) {
+                iStart = 1;
+                iEnd = iListLength;
+            } else if (oPaging.iPage >= (oPaging.iTotalPages - iHalf)) {
+                iStart = oPaging.iTotalPages - iListLength + 1;
+                iEnd = oPaging.iTotalPages;
+            } else {
+                iStart = oPaging.iPage - iHalf + 1;
+                iEnd = iStart + iListLength - 1;
+            }
+
+            for (i = 0, iLen = an.length; i < iLen; i += 1) {
+                // Remove the middle elements
+                $('li:gt(0)', an[i]).filter(':not(:last)').remove();
+
+                // Add the new list items and their event handlers
+                for (j = iStart; j <= iEnd; j += 1) {
+                    sClass = (j === oPaging.iPage + 1) ? 'class="active"' : '';
+                    $('<li ' + sClass + '><a href="#">' + j + '</a></li>')
+                        .insertBefore($('li:last', an[i])[0])
+                        .bind('click', jcf);
+                }
+
+                // Add / remove disabled classes from the static elements
+                if (oPaging.iPage === 0) {
+                    $('li:first', an[i]).addClass('disabled');
+                } else {
+                    $('li:first', an[i]).removeClass('disabled');
+                }
+
+                if (oPaging.iPage === oPaging.iTotalPages - 1 || oPaging.iTotalPages === 0) {
+                    $('li:last', an[i]).addClass('disabled');
+                } else {
+                    $('li:last', an[i]).removeClass('disabled');
+                }
+            }
+        }
+    }
+});
+
 // fonction permettant de cliquer sur l'id new_line_link avec la combinaison
 // de touches Ctrl+N
 //var isCtrl=false;
@@ -24,12 +123,19 @@ jQuery(function (){
 //    }
 //});
 
+// instruction de data-tables pour s'adapter à bootstrap'
+//$.extend($.fn.dataTableExt.oStdClasses, {
+//    "sWrapper": "dataTables_wrapper form-inline"
+//});
+
 
 // mise en forme des tables de lignes
 jQuery(function() {
     if ($('.public_lines .data_table').length != 0) {
         var oTable= $('.public_lines .data_table').dataTable(
         {
+            "sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+            "bAutoWidth": false,
             "oLanguage": {
                 "sUrl": "/frenchdatatable.txt"
             },
@@ -38,7 +144,7 @@ jQuery(function() {
             null,
             null,
             null,
-             null,
+            null,
             null,
             null,
 
