c10608a9b09d611a8dd19dd4731c2c99fe852078
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-04-14 07:35:27 +0200

    factorisation des menu helper de la zone admin

--- app/helpers/admin/organisms_helper.rb
+++ app/helpers/admin/organisms_helper.rb
@@ -2,32 +2,17 @@
 
 module Admin::OrganismsHelper
 
-   # menu books prend une collection de livres et crée les entrées de menu correspondants
-  # join permet de séparer les entrées entre les différents livres
-  def admin_menu_books(books)
-    content_tag(:ul, :class=>"dropdown-menu") do
-      html = []
-      html << content_tag(:li) { link_to 'Listes des livres', admin_organism_books_path(@organism) } +
-              content_tag(:li) {link_to 'Nouveau', new_admin_organism_book_path(@organism)}
-      html.join(content_tag(:li, :class=>"divider"){}).html_safe
-    end
-  end
+   # admin_menu books prend une chaine de caractère représentant un modèle
+   # et crée les entrées Afficher et Nouveau
 
-def admin_menu_banks(banks)
+  def admin_menu(model)
     content_tag(:ul, :class=>"dropdown-menu") do
-      content_tag(:li) {link_to 'Listes des comptes', admin_organism_bank_accounts_path(@organism)} +
-      content_tag(:li) { link_to 'Nouveau', new_admin_organism_bank_account_path(@organism)}
+      content_tag(:li) { link_to 'Afficher', eval("admin_organism_#{model.pluralize}_path(@organism)") } +
+      content_tag(:li) {link_to 'Nouveau', eval("new_admin_organism_#{model}_path(@organism)") }
     end
-     
   end
   
-def admin_menu_cashes(cashes)
-    content_tag(:ul, :class=>"dropdown-menu") do
-       content_tag(:li) {link_to 'Listes des comptes', admin_organism_cashes_path(@organism)} +
-       content_tag(:li) { link_to 'Nouveau', new_admin_organism_cash_path(@organism)}
-     end
-  end
-
+ 
 
 
 end
