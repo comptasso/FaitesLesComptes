d1e477b4c7149323aa8ae07471eab9cd5938ffe9
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-05-12 07:19:35 +0200

    first spec de bank_extract_line

---
+++ spec/models/bank_extract_line_spec.rb.rb
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+require 'spec_helper'
+
+describe BankExtractLine do
+  include OrganismFixture
+
+  before(:each) do
+    create_minimal_organism
+    @be = @ba.bank_extracts.create!(:begin_date=>Date.today.beginning_of_month,
+      end_date:Date.today.end_of_month,
+      begin_sold:1,
+      end_sold:2,
+      locked:false)
+    @l = Line.create!(narration:'bel', line_date:Date.today, debit:7, credit:0, payment_mode:'Espèces', cash_id:@ca.id, book_id:@ob.id, nature_id:@n.id)
+  end
+
+  def valid_attributes
+    {bank_extract_id:@be.id, blid:@l.id}
+  end
+
+  describe 'with valid attributes' do
+    it 'is created' do
+      bel = @be.bank_extract_lines.new(valid_attributes)
+      bel.should be_valid
+    end
+  end
+
+end
