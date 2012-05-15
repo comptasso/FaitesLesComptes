c82930fc3a5d36784f383eb98970de590c474e6d
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-05-15 22:43:44 +0200

    ajout du fichier rowReordering.js

--- app/assets/javascripts/bank_extract_lines.js
+++ app/assets/javascripts/bank_extract_lines.js
@@ -1,21 +1,22 @@
 jQuery(function() {
+  // masquer les lignes de détail à l'affichage'
   $('.public_bank_extract_lines .row_detail').hide();
 
+  // associer le click au span
   $('.public_bank_extract_lines .toggle-detail').click(function () {
     // identifier l'id de l'objet,
       var objid = $(this).attr('id');
     // construire une variable avec l'id pour avoir la classe  des rangées de détail
       var classid = '.' + objid;
-      $('.champ h3 ').html(classid);
-       $(classid).toggle();
+      $(classid).toggle();
        if ($(classid).is(':visible')) {
          $(this).html("&#x229F;");
        } else
          {
           $(this).html("&#x229E;");
          }
-  })
+  });
 
-
-
+  $('.public_bank_extract_lines .data_table').dataTable()
+  .rowReordering();
 });
