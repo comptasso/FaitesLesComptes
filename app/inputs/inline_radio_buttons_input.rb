554bdf0013dfcd141c9e4f5243403619a8428c7a
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-06-24 08:48:03 +0200

    commenté inputs/inline_radio_buttons

--- app/inputs/inline_radio_buttons_input.rb
+++ app/inputs/inline_radio_buttons_input.rb
@@ -1,17 +1,24 @@
 # classe destinée à permettre un affichage inline des radio buttons
+# bootstrap indique que le lable doit avoir la classe inline
+# mais simple_form as: radio_buttons donne la classe radio qui ne semble pas
+# pouvoir être complétée facilement.
+#
+# Il suffit maintenant de faire :as=>:inline_radio_buttons
 #
 
 class InlineRadioButtonsInput < SimpleForm::Inputs::CollectionRadioButtonsInput
   def item_wrapper_class
-        "radio inline"
-      end
+    "radio inline"
+  end
 
+  # on surcharge également input car @builder.send de radio buttons
+  # ne trouve pas la collection_radio_buttons
   def input
-        label_method, value_method = detect_collection_methods
+    label_method, value_method = detect_collection_methods
 
-        @builder.send("collection_radio_buttons",
-          attribute_name, collection, value_method, label_method,
-          input_options, input_html_options, &collection_block_for_nested_boolean_style
-        )
-      end
+    @builder.send("collection_radio_buttons",
+      attribute_name, collection, value_method, label_method,
+      input_options, input_html_options, &collection_block_for_nested_boolean_style
+    )
+  end
 end
