a7160b78ca3013c87f919360df9c6db817b2d640
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-06-30 07:11:37 +0200

    revu l affichage des tables dans la partie admin avec création dun fihcier percentage.css pour pouvoir facilement fixer les largeurs des colonnes de tables

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,6 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require jquery.dataTables
-//= require tabletools/TableTools
 //= require jquery.jqplot.min
 //= require jqplot
 //= require_tree .
