a18d7c8694f776aac1ab460c9751aa2f31ba1c5e
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-07-01 09:03:15 +0200

    spec sur la vue index des cash controls

--- spec/views/cash_controls/index.html.erb_spec.rb
+++ spec/views/cash_controls/index.html.erb_spec.rb
@@ -4,17 +4,24 @@
 
 require 'spec_helper'
 
-describe "cash_lines/index" do
+#RSpec.configure do |c|
+#  c.filter = {:wip=>true}
+#end
+
+
+describe "cash_controls/index" do
   include JcCapybara
 
-  let(:o) {mock_model(Organism, title: 'spec cd')}
+  let(:o) {mock_model(Organism, title: 'spec cd')} 
   let(:p) {mock_model(Period, :start_date=>Date.today.beginning_of_year, :close_date=>Date.today.end_of_year)}
   let(:c) {mock_model(Cash, name: 'Magasin')}
-  let(:cc1) {mock_model(CashControl, date:1.week.ago, amount:10.52)}
-  let(:cc2) {mock_model(CashControl, date:Date.today, amount:99.08)}
-#  let(:n) {mock_model(Nature, :name=>'achat de marchandises')}
-#  let(:cl1) { mock_model(Line, :line_date=>Date.today, :narration=>'test', :debit=>'45', :nature_id=>n.id)}
-#  let(:cl2) {mock_model(Line, :line_date=>Date.today, :narration=>'autre ligne', :debit=>'54', :nature_id=>n.id)}
+  let(:cc1) {mock_model(CashControl, date:1.week.ago.to_date, amount:10.52, cash_id:c.id, locked?:false, cash:c)}
+  let(:cc2) {mock_model(CashControl, date:Date.today, amount:99.08, cash_id:c.id, locked?:false, cash:c)}
+  
+#  it 'current_page' , :wip=>true do
+#      render
+#      current_page?(:mois=> 2).should == true
+#    end
 
 
   before(:each) do
@@ -22,9 +29,10 @@ describe "cash_lines/index" do
     assign(:period, p)
     assign(:cash, c)
     assign(:cash_controls, [cc1,cc2])
-
     p.stub(:list_months).and_return %w(jan fév mar avr mai jui jui aou sept oct nov déc)
-
+    c.stub(:sold).with(Date.today).and_return(102)
+    c.stub(:sold).with(1.week.ago.to_date).and_return(10.52)
+    view.stub("current_page?").and_return false
   end
 
 
@@ -49,22 +57,24 @@ describe "cash_lines/index" do
     it 'chaque ligne a deux icones (edit et lock)' do
       page.all("tbody tr").each do |row|
         row.find("td:last").all('img').first[:src].should == '/assets/icones/modifier.png'
-        row.find("td:last").all('img').first[:src].should == '/assets/icones/lock.png'
+        row.find("td:last").all('img').last[:src].should == '/assets/icones/verrouiller.png'
       end
     end
+   
 
-    it 'affiche l ecart de caisse' 
+    it 'affiche l ecart de caisse'  
   end
 
   context 'avec un cash_control locked' do
     before(:each) do
        cc1.stub(:locked?).and_return(true)
+       assign(:cash_controls, [cc1,cc2])
        render
     end
 
     it 'une cash line locked n a pas d icone pour lock' do
-      page.find("tbody tr:first td:last").should_not have_icon('lock.png')
-      page.find("tbody tr:last td:last").should have_icon('lock.png')
+      page.find("tbody tr:first td:last").should_not have_icon('verrouiller')
+      page.find("tbody tr:last td:last").should have_icon('verrouiller')
     end
   end
 
