287e0c0decad6c59342a3e74a642702cd04a3c74
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-07-05 06:39:57 +0200

    cadenas.png à la place de cle.png pour les actions verrouiller plus corrections sur le menu caisse et ordre des transferts dans la vue index

--- app/assets/javascripts/transfers.js
+++ app/assets/javascripts/transfers.js
@@ -6,12 +6,22 @@
 $(document).ready(function() {
 	$('#transfer-table').dataTable( {
     "sDom": 'lfrtip',
-
 		"sPaginationType": "bootstrap",
 		"oLanguage": {
           "sUrl": "/frenchdatatable.txt"   // ce fichier est dans /public
 
-		}
+		},
+     "aoColumnDefs": [
+    {
+      "bSortable": false,
+      "aTargets": ['actions' ]
+    },
+    {
+      "sType": "date-euro",
+      "asSortable": ['asc', 'desc'],
+      "aTargets": ['date-euro'] // les colonnes date au format français ont la classe date-euro
+    }]
+
 
 	} );
 } );
