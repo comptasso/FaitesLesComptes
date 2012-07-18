577bb8fcafce5f63705d040b043c36f6be3a4670
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-07-18 15:01:40 +0200

    suppression des actions show et index de organism dans la partie compta qui redirige vers balance - le menu compta envoie par défaut vers balance

--- app/controllers/compta/organisms_controller.rb
+++ app/controllers/compta/organisms_controller.rb
@@ -1,19 +1,7 @@
 # -*- encoding : utf-8 -*-
 
 class Compta::OrganismsController < Compta::ApplicationController
-  # GET /organisms
-  # GET /organisms.json
- 
-
-  def index
-    @organisms = Organism.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @organisms }
-    end
-  end
-
+  
   # GET /organisms/1
   # GET /organisms/1.json
   def show
@@ -34,73 +22,11 @@ class Compta::OrganismsController < Compta::ApplicationController
       @period = @organism.periods.last
       session[:period]=@period.id
     end
+    redirect_to compta_period_balance_path(@period)
+   
+  end
+
   
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @organism }
-    end
-  end
-
-  # GET /organisms/new
-  # GET /organisms/new.json
-  def new
-    @organism = Organism.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @organism }
-    end
-  end
-
-  # GET /organisms/1/edit
-  def edit
-    @organism = Organism.find(params[:id])
-  end
-
-  # POST /organisms
-  # POST /organisms.json
-  def create
-    @organism = Organism.new(params[:organism])
-
-    respond_to do |format|
-      if @organism.save
-        format.html { redirect_to new_admin_organism_period_url(@organism), notice: "Création de l'organisme effectuée, un livre des recettes et un livre des dépenses ont été créés.\n
-          Il vous faut maintenant créer un exercice pour cet organisme" }
-        format.json { render json: @organism, status: :created, location: @organism }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @organism.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /organisms/1
-  # PUT /organisms/1.json
-  def update
-    @organism = Organism.find(params[:id])
-
-    respond_to do |format|
-      if @organism.update_attributes(params[:organism])
-        format.html { redirect_to [:admin, @organism], notice: "Modification de l'organisme effectuée" }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @organism.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /organisms/1
-  # DELETE /organisms/1.json
-  def destroy
-    @organism = Organism.find(params[:id])
-    @organism.destroy
-
-    respond_to do |format|
-      format.html { redirect_to admin_organisms_url }
-      format.json { head :ok }
-    end
-  end
 
  
 end
