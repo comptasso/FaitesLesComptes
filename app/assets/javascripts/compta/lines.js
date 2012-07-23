9db82e03dd8fef2d7f450d3c89edc23bf65bd674
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-07-23 18:50:32 +0200

    reformaté lines.js pour listing et modif de create.js.erb pour redessiner la data_table

--- app/assets/javascripts/compta/lines.js
+++ app/assets/javascripts/compta/lines.js
@@ -1,89 +1,66 @@
-
-//
-//// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-// You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
-
-jQuery(function (){
-    $('.decimal').live('focus', $f_empty); //vide le champ s'il est à zero (pour faciliter la saisie)
-    $('.decimal').live('blur',$f_zero); // met zero dans le champ s'il est vide
-});
+"use strict";
+/*jslint browser: true */
+var jQuery, $, stringToFloat;
 
 
-// mise en forme des tables de lignes
-jQuery(function() {
-    
-         $('.account_lines_table').dataTable( {
-            "sDom": "lfrtip",
-            "bAutoWidth": false,
-            "sPaginationType": "bootstrap",
-            "oLanguage": {
-                "sUrl": "/frenchdatatable.txt"
-            },
-              "aoColumnDefs": [
-       {
-      "sType": "date-euro",
-      "asSortable": ['asc', 'desc'],
-      "aTargets": ['date-euro'] // les colonnes date au format français ont la classe date-euro
-    }],
-           
-            "iDisplayLength": 15,
-            "aLengthMenu": [[10, 15, 25, 50, -1], [10, 15, 25, 50, "Tous"]],
-            "bStateSave": true,
-"fnStateSave": function (oSettings, oData) {
-                localStorage.setItem('DataTables_' + window.location.pathname, JSON.stringify(oData));
-            },
-            "fnStateLoad": function (oSettings) {
-                return JSON.parse(localStorage.getItem('DataTables_' + window.location.pathname));
-            },
-            "fnFooterCallback": function ( nRow, aaData, iStart, iEnd, aiDisplay ) {
-                /*
+
+function drawDataTable() {
+    $('.listing_data_table').dataTable({
+        "sDom": "lfrtip",
+        "bAutoWidth": false,
+        "sPaginationType": "bootstrap",
+        "oLanguage": {
+            "sUrl": "/frenchdatatable.txt"
+        },
+        "aoColumnDefs": [
+            {
+                "sType": "date-euro",
+                "asSortable": ['asc', 'desc'],
+                "aTargets": ['date-euro'] // les colonnes date au format français ont la classe date-euro
+            }],
+        "iDisplayLength": 15,
+        "aLengthMenu": [[10, 15, 25, 50, -1], [10, 15, 25, 50, "Tous"]],
+        "bStateSave": true,
+        "fnStateSave": function (oSettings, oData) {
+            localStorage.setItem('DataTables_' + window.location.pathname, JSON.stringify(oData));
+        },
+        "fnStateLoad": function (oSettings) {
+            return JSON.parse(localStorage.getItem('DataTables_' + window.location.pathname));
+        },
+        "fnFooterCallback": function (nRow, aaData, iStart, iEnd, aiDisplay) {
+            /*
              * Calculate the total market share for all browsers in this table (ie inc. outside
              * the pagination)
              */
-                var iTotalDebit = 0;
-                for ( var i=0 ; i<aaData.length ; i++ )
-                {
-                    iTotalDebit += stringToFloat(aaData[i][5]);
-                }
-
-                /* Calculate the market share for browsers on this page */
-                var iPageDebit = 0.0
-                for ( var i=iStart ; i<iEnd ; i++ )
-                {
-                    iPageDebit += stringToFloat(aaData[aiDisplay[i] ][5]);
-                }
-
-                var iTotalCredit = 0.0
-                for ( var i=0 ; i<aaData.length ; i++ )
-                {
-                    iTotalCredit += stringToFloat(aaData[i][6]);
-                }
-
-                /* Calculate the market share for browsers on this page */
-                var iPageCredit = 0.0;
-                for ( var i=iStart ; i<iEnd ; i++ )
-                {
-                    iPageCredit += stringToFloat(aaData[ aiDisplay[i] ][6]);
-                }
-
-                /* Modify the footer row to match what we want */
-                var nCells = nRow.getElementsByTagName('th');
-                nCells[1].innerHTML =  iPageDebit.toFixed(2) +'<br/>'+ iTotalDebit.toFixed(2);
-                nCells[2].innerHTML =  iPageCredit.toFixed(2)+'<br/>'+ iTotalCredit.toFixed(2);
-
+            var iTotalDebit = 0, i = 0, iPageDebit = 0.0, iTotalCredit = 0.0, iPageCredit = 0.0, nCells;
+            for (i = 0; i < aaData.length; i += 1) {
+                iTotalDebit += stringToFloat(aaData[i][5]);
             }
 
-        });
+            /* Calculate the market share for browsers on this page */
+            for (i = iStart; i < iEnd; i += 1) {
+                iPageDebit += stringToFloat(aaData[aiDisplay[i]][5]);
+            }
 
+            for (i = 0; i < aaData.length; i += 1) {
+                iTotalCredit += stringToFloat(aaData[i][6]);
+            }
 
-//        $('td', oTable.fnGetNodes()).hover( function() {
-//            var iCol = $('td', this.parentNode).index(this) % 5;
-//            var nTrs = oTable.fnGetNodes();
-//            $('td:nth-child('+(iCol+1)+')', nTrs).addClass( 'highlighted' );
-//        }, function() {
-//            $('td.highlighted', oTable.fnGetNodes()).removeClass('highlighted');
-//        } );
- 
+            /* Calculate the market share for browsers on this page */
+            for (i = iStart; i < iEnd; i += 1) {
+                iPageCredit += stringToFloat(aaData[aiDisplay[i]][6]);
+            }
+
+            /* Modify the footer row to match what we want */
+            nCells = nRow.getElementsByTagName('th');
+            nCells[1].innerHTML =  iPageDebit.toFixed(2) + '<br/>' + iTotalDebit.toFixed(2);
+            nCells[2].innerHTML =  iPageCredit.toFixed(2) + '<br/>' + iTotalCredit.toFixed(2);
+
+        }
+    });
+}
+
+// mise en forme des tables de lignes
+jQuery(function () {
+    drawDataTable();
 });
- 
