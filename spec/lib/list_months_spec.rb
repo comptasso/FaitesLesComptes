34efceaeaef220a184b50b09163455b78d821e80
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-07-30 08:21:15 +0200

    refait un spec mal conçu pour ListMonths et effacé compta/views/lines_spec

--- spec/lib/list_months_spec.rb
+++ spec/lib/list_months_spec.rb
@@ -4,8 +4,10 @@ require 'list_months'
 
 describe 'list_months' do
   it 'crée un nombre de MonthYear correspondant au nombre de mois' do
-    m = ListMonths.new((1.year.ago.to_date) + 1, Date.today)
-    m.count.should == 12
+    d = Date.today.beginning_of_month
+    e = d.years_since(1)-1
+    m = ListMonths.new(d,e)
+    m.count.should == 12 
   end
 
 end
