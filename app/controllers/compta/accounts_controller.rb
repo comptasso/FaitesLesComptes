21ceb31b54b0df85d6dad610e8cddf9bdeef965e
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-08-01 07:54:56 +0200

    début de spec de la vue compta#accounts"index

--- app/controllers/compta/accounts_controller.rb
+++ app/controllers/compta/accounts_controller.rb
@@ -1,13 +1,15 @@
 # -*- encoding : utf-8 -*-
+
 class Compta::AccountsController < Compta::ApplicationController
   # GET /compta/accounts
   # GET /compta/accounts.json
+  # index affiche le plan comptable pour l'exercice
   def index
-    @compta_accounts = @period.accounts.all
+    @compta_accounts = @period.accounts
 
     respond_to do |format|
       format.html # index.html.erb
-      format.pdf
+      format.pdf {send_data Account.to_pdf(@period).render, filename:'Plan_comptable.pdf', disposition:'inline'}
       format.json { render json: @compta_accounts }
     end
   end
--- spec/views/compta/accounts/index.html.erb_spec.rb
+++ spec/views/compta/accounts/index.html.erb_spec.rb
@@ -1,21 +1,32 @@
 require 'spec_helper'
 
 describe "compta/accounts/index" do
+  include JcCapybara
+  let(:p) {mock_model(Period, start_date:Date.today.beginning_of_year, end_date:Date.today.end_of_year)}
+
+
   before(:each) do
-    assign(:accounts, [
+    assign(:period, p)
+    assign(:compta_accounts, [
       stub_model(Account,
         :number => "60",
         :title => 'Compte achats' 
       ),
       stub_model(Account,
-        :number => "60",
+        :number => "603",
         :title => 'Stocks'
       )
     ])
+  Account.any_instance.stub(:period).and_return(p)
   end
 
   it "renders a list of accounts" do 
     render
-    assert_select "tr>td", :text => "60".to_s, :count => 2
+    assert_select "tr", :count => 3 # la ligne de titre et les deux lignes de comptes
+    page.find('tbody tr:first td:first').should have_content('60')
+    page.find('tbody tr:last td:first').should have_content('603')
   end
+
+  it 'affiche si le compte est utilisé'
+  it 'affiche l icone pdf pour imprimer'
 end
