1c4e9ee3a64068e932e9b07352e052350f31f9b3
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-08-22 13:03:20 +0200

    supprimé l action et la vue index pour Periods (dans le zone saisie/consult)

--- app/controllers/periods_controller.rb
+++ app/controllers/periods_controller.rb
@@ -3,20 +3,8 @@
 
 class PeriodsController < ApplicationController
 
-# FIXME : dans la vue index : quand un exercice est fermé, l'action cloture ne doit rien proposer
 
-  # GET /periods
-  # GET /periods.json
-  def index
-    @periods = @organism.periods.all
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @periods }
-    end
-  end
-
-  # change d'exercice sans pour autant afficher l'exercice concerné mais ne marche pas
-  # FIXME
+  # change d'exercice sans pour autant afficher l'exercice concerné 
   # utile par exemple quand on veut rester dans les livres en changeant d'exercice
   def change
     Rails.logger.info "Dans la méthode change avec params[:id] = #{params[:id]}"
