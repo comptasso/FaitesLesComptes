5613ae08f3ef406c7486f6a5994197ac6a561086
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-08-23 19:03:49 +0200

    suppression de l action archive dans admin pour periods_controller

--- app/controllers/admin/periods_controller.rb
+++ app/controllers/admin/periods_controller.rb
@@ -118,38 +118,7 @@ class Admin::PeriodsController < Admin::ApplicationController
     redirect_to admin_organism_period_accounts_path(@organism,@period)
   end
 
-  def archive
-     tmp_file="#{Rails.root}/tmp/#{@organism.title}_#{@period.exercice}.jcl"
-      # Créer un fichier : y écrirer les infos de l'exercice
-      File.open(tmp_file, 'w') do |f|
-        f.write @organism.to_yaml
-        f.write @period.to_yaml
-        f.write @organism.destinations.all.to_yaml
-        f.write @organism.natures.all.to_yaml
-        f.write @organism.bank_accounts.all.to_yaml
-        @organism.bank_accounts.all.each do |b|
-          f.write b.bank_extracts.all.to_yaml
-          b.bank_extracts.all.each do |be|
-            f.write be.bank_extract_lines.all.to_yaml
-          end
-          f.write b.check_deposits.all.to_yaml
-        end
-        f.write @organism.books.all.to_yaml
-        @organism.books.all.each do |b|
-          f.write b.lines.all.to_yaml
-        end
-        f.write @organism.cashes.all.to_yaml
-        @organism.cashes.all.each do |c|
-          f.write c.cash_controls.all
-        end
-        f.write @period.accounts.all.to_yaml
-      end
-
-    send_file tmp_file, type: 'application/jcl'
-     
-  end
-
- 
+   
   # action de cloture d'un exercice
   # problématique de confirmation
   def close
