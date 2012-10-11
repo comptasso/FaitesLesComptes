4e92808d7915a888c8cfa95dceb08cfaa4ef86a5
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-10-11 07:25:03 +0200

    transformé les views#lines_spec en in_out_writings_spec

--- spec/views/in_out_writings/_form.html.erb_spec.rb
+++ spec/views/in_out_writings/_form.html.erb_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-describe "lines/new" do  
+describe "in_out_writings/new" do
   include JcCapybara
 
 
@@ -13,10 +13,9 @@ describe "lines/new" do
   let(:d) {stub_model(Destination, name:'destination')}
 
   before(:each) do
-
-    assign(:line, stub_model(Line,
-        :line_date => Date.today, ref:nil
-      ).as_new_record)
+ assign(:in_out_writing, stub_model(InOutWriting, book_id:1, date:Date.today).as_new_record)
+    assign(:line, stub_model(ComptaLine, ref:nil).as_new_record)
+    assign(:counter_line, stub_model(ComptaLine).as_new_record)
     assign(:book, stub_model(IncomeBook, :title=>'Recettes'))
     assign(:period, stub_model(Period, start_date: Date.civil(2012,01,01), close_date: Date.civil(2012,12,31)) )
     assign(:organism, o)
@@ -26,12 +25,12 @@ describe "lines/new" do
 
   it 'le champs nature doit être précédé d une étoile' do
     render
-    page.find(:xpath, '//label[@for="line_nature_id"]').should have_content('*')
+    page.find(:xpath, '//label[@for="in_out_writing_compta_lines_attributes_0_nature_id"]').should have_content('*')
   end
 
   it 'le champ paiement doit être précédé d une étoile' do
     render
-    page.find(:xpath, '//label[@for="line_payment_mode"]').should have_content('*')
+    page.find(:xpath, '//label[@for="in_out_writing_compta_lines_attributes_0_payment_mode"]').should have_content('*')
   end
 
   it 'test en cas d erreur dans le remplissage' do
--- spec/views/in_out_writings/new.html.erb_spec.rb
+++ spec/views/in_out_writings/new.html.erb_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-describe "lines/new" do 
+describe "in_out_writings/new" do
   include JcCapybara
 
 
@@ -13,10 +13,9 @@ let(:n) {stub_model(Nature, name:'nature')}
 let(:d) {stub_model(Destination, name:'destination')}
 
 before(:each) do
-
-    assign(:line, stub_model(Line,
-      :line_date => Date.today, ref:nil
-    ).as_new_record)
+    assign(:in_out_writing, stub_model(InOutWriting, book_id:1, date:Date.today).as_new_record)
+    assign(:line, stub_model(ComptaLine, ref:nil).as_new_record)
+    assign(:counter_line, stub_model(ComptaLine).as_new_record)
     assign(:book, stub_model(IncomeBook, :title=>'Recettes'))
     assign(:period, stub_model(Period, start_date: Date.civil(2012,01,01), close_date: Date.civil(2012,12,31)) )
     assign(:organism, o)
@@ -27,16 +26,18 @@ before(:each) do
   it "renders new line  form" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-      assert_select "form", :action => book_lines_path(book), :method => "post" do
-      assert_select "input#line_line_date_picker", :name => "line[line_date_picker]"
+      assert_select "form", :action => book_in_out_writings_path(book), :method => "post" do
+      assert_select "input#in_out_writing_date_picker", :name => "in_out_writing[date_picker]"
     end
   end
 
   it 'give the info in a notice if previous line' do
+    @w = mock_model(InOutWriting, book:book, book_id:1, date:Date.today)
     view.stub(:icon_to).and_return('stub icone')
-    assign(:previous_line, @pl = stub_model(Line, id:1, narration:'test',
+    assign(:previous_line, @pl = stub_model(ComptaLine, id:1, narration:'test',
         book_id:book.id, debit:0, credit:12, line_date:Date.today, nature:n, destination:d))
-    @pl.stub(:support).and_return('DX')
+    @pl.stub(:owner).and_return @w
+    @w.stub(:support).and_return('DX')
     @pl.stub(:editable?).and_return(true)
     render
     rendered.should have_content 'Ligne n°1 créée'
