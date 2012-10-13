f502dce34b6eed4a4e64b0f5aaa913fbc9e84b63
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-10-13 19:02:48 +0200

    destination utilise maintenant compta_lines

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,18 +1,18 @@
 # -*- encoding : utf-8 -*-
 
-class Destination < ActiveRecord::Base
+class Destination < ActiveRecord::Base 
   belongs_to :organism
-  has_many :lines
+  has_many :compta_lines
   validates :organism_id, :presence=>true
-  validates :name, :presence=>true, :uniqueness=>{:scope=>:organism_id}
- default_scope order: 'name ASC'
+  validates :name, :presence=>true, uniqueness:true
+  default_scope order: 'name ASC'
 
   before_destroy :ensure_no_lines
 
   private
 
   def ensure_no_lines
-    if lines.empty?
+    if compta_lines.empty?
       return true
     else
       errors.add(:base, 'Des écritures font référence à cette destination')
