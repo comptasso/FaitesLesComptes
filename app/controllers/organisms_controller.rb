f730326c41696cd71899acc88f1268329e762ed4
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-10-25 07:03:53 +0200

    méthode guess_date dans organism_controller pour gérer aussi bien les exercices passés que futurs

--- app/controllers/organisms_controller.rb
+++ app/controllers/organisms_controller.rb
@@ -21,9 +21,8 @@ class OrganismsController < ApplicationController
       return
     end
     
-    # Ce min est nécessaire car il y a un problème avec les soldes si la date du jour est postérieure à la date de clôture
-    # du dernier exercice - probablement il faut trouver plus élégant
-    @date=[@organism.periods.last.close_date, Date.today].min
+   
+    @date=guess_date
 
     # Construction des éléments des paves
     @paves=[]
@@ -40,6 +39,16 @@ class OrganismsController < ApplicationController
 
 
   protected
+
+  # trouve la meilleure date pour l'affichage du dashboard
+  # soit le date du jour s'il y a un exercice correspondant (via la méthode find_period)
+  # soit la date la plus proche à partir des exercices passés ou futurs
+  def guess_date
+    return Date.today if @organism.find_period
+    # l'exercice est-il futur
+    direction  =  @organism.periods.first.close_date.past?
+    direction ? @organism.periods.last.close_date : @organism.periods.first.start_date
+  end
 
 
   def cash_books
