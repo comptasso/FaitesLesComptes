c5d6b32e9fc21502344c63404e636f9abc928840
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-10-27 13:44:25 +0200

    première ébauche de GeneralLedger

--- app/models/compta/general_ledger.rb
+++ app/models/compta/general_ledger.rb
@@ -6,6 +6,21 @@ module Compta
   # reprenant mois par mois les soldes des différentes journaux
   # le GeneralLedger se construit avec un period et s'appuie sur MonthlyLedger
    class GeneralLedger 
+      def initialize(period)
+        @period = period
+      end
 
+      # les lignes d'un journal général ne sont que la compilation des lignes des différents
+      # MonthlyLedger du period
+      def lines
+        tableau = []
+        @period.list_months.map do |my|
+          cml = Compta::MonthlyLedger.new(@period, my)
+          tableau << cml.title_line
+          cml.lines.each {|l| tableau << l}
+          tableau << cml.total_line
+        end
+        tableau
+      end
    end
 end
