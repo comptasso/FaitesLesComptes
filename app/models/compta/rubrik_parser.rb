640f1a8d7839fb6aabca7a4b4de328a2612c93a3
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2012-12-24 06:36:25 +0100

    documentation de rubrikparser

--- app/models/compta/rubrik_parser.rb
+++ app/models/compta/rubrik_parser.rb
@@ -2,25 +2,36 @@
 
 module Compta
   #
-  # Cette classe prend en argument un exercice et un tableau de chaine de caractères
-  # Il retourne une collection de RubrikLine.
+  # Cette classe prend en argument un exercice et un tableau de chaine de caractères et
+  # retourne une collection de RubrikLine.
+  #
   # Le parsing prend par défaut l'argument % en fin de numéro
-  # - indique que l'on cherche un amortissement ou une dépréciation.
-  # 
-  # ! indique que le compte doit être retiré de la liste
-  # D indique que l'on prend ce compte que s'il est débiteur
-  # C indique que l'on prend ce compte que s'il est créditeur
+  # * le signe - indique que l'on cherche un amortissement ou une dépréciation.
+  # * ! indique que le compte doit être retiré de la liste
+  # * D indique que l'on prend ce compte que s'il est débiteur
+  # * C indique que l'on prend ce compte que s'il est créditeur
   #
   # Le but est par exemple d'avoir une série de lignes
   # avec avec le numéro de compte principal, l'intitulé principal
-  # le montant brut, l'amortissement
+  # le montant brut, l'amortissement.
   #
-  # Ainsi 20 renvoie l'ensemble des comptes 20
-  # 20, 201 doit renvoyer l'ensemble des comptes 20 mais pas compter le 201 deux fois
-  # 20, !208, doit prendre tous les comptes 20 mais pas le 208
-  # 20, -280, doit prendre tous les comptes 20 et tous les comptes 280 mais avec colon_1 mis à false
-  # 20, 201D, doit prendre le compte 201 que s'il est débiteur
+  # Le premier des numéros sert de numéro de compte et de libellé principal.
   #
+  # === Exemples :
+  # * 20 renvoie l'ensemble des comptes 20
+  # * 20, 201 doit renvoyer l'ensemble des comptes 20 mais pas compter le 201 deux fois
+  # * 20, !208, doit prendre tous les comptes 20 mais pas le 208
+  # * 20, -280, doit prendre tous les comptes 20 et tous les comptes 280 mais avec colon_1 mis à false
+  # * 20, 201D, doit prendre le compte 201 que s'il est débiteur
+  #
+  # === Variables d'instance
+  # La classe utilise plusieurs variables d'instance pour stocker les numéros de
+  # comptes à reprendre :
+  # [@select_nums] Cas le plus général des numéros de comptes sélectionnés
+  # [@col2_nums] pour les comptes qui doivent s'inscrire en deuxième colonne d'un document (amortissement ou provision à l'actif d'un bilan)
+  # [@credit_nums] pour les comptes qui ne sont retenus que lorsqu'ils sont créditeurs
+  # [@debit_nums] pour les comptes qui ne sont retenus que lorsqu'ils sont débiteurs
+  # 
   #
   class RubrikParser
 
@@ -29,11 +40,14 @@ module Compta
     attr_reader :list
 
     # le RubrikParser s'initialise avec un exercice et une chaine de caractères
-    #  représentant une liste de numéros par exemple '20 -280'
-    # première étape, il identifie les numéros de comptes dont il aura besoin
-    # deuxième étape, il crée les RubrikLine correspondantes
+    # représentant une liste de numéros par exemple '20 -280'
+    # * première étape, il identifie les numéros de comptes dont il aura besoin
+    # * deuxième étape, il crée les RubrikLine correspondantes
     # il fait ça pour les deux exercices (celui demandé et précédent s'il existe)
-    
+    #
+    # A la fin de initialize, on a une variable d'instance @list
+    # qui contient un tableau de hash ordonné
+    # il suffit d'appeler la méthode rubrik_lines pour avoir les lignes de rubriques
     def initialize(period, sens, args)
       @period = period
       # TODO Les rejets doivent être à la fin
@@ -46,10 +60,8 @@ module Compta
       # ceci nous donne tous les comptes des deux périodes
       @numbers = @period.two_period_account_numbers
       set_numbers # déclanche le parsing
-      set_lines # trie et réordonne les numéros trouvés
-      # à la fin de initialize, on a une variable d'instance @list
-      # qui contient un tableau de hash ordonné
-      # il suffit d'appeler rubrik_lines pour avoir les lignes de rubriques
+      set_lines # construit et réordonne les numéros trouvés
+      
     end
     
     # construit et renvoie la série des rubrik_lines
@@ -63,12 +75,10 @@ module Compta
       @list.map {|l| l[:num]}
     end
 
-
-
-
-
     protected
 
+    # appelle parse_numbers pour chacun des éléments de la liste fournie 
+    # en argument de l'instance de RubrikLine
     def set_numbers
       @numeros.each {|num| parse_num(num)}
     end
@@ -105,7 +115,8 @@ module Compta
     end
 
     
-
+    # prend un élément de la liste et selon sa constitution appelle la méthode
+    # qui permet de l'ajouter à l'une des variables d'instance de la classe
     def parse_num(num)
       case  num
       when  /^\d*$/ then add_numbers(num)
@@ -127,7 +138,7 @@ module Compta
       @select_nums += @numbers.select {|n| n =~ /^#{num}\d*/ && !n.in?(@select_nums) }
     end
 
-    # on rejeète le numéro de la liste générale
+    # on rejète le numéro de la liste générale
     # et on l'ajoute dans celle de col2
     def add_col2_numbers(num)
       @select_nums.reject! {|n| n =~ /^#{num}\d*/}
