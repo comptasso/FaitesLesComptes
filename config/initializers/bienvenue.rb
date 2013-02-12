7132facad060f799c492200a84ae5e3d9844118f
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-02-12 20:39:27 +0100

    modifié les messages d accueil

--- config/initializers/bienvenue.rb
+++ config/initializers/bienvenue.rb
@@ -1,4 +1,10 @@
 # coding: utf-8
 
+
 # sans accent pour éviter les problèmes d'affichage sous windows
-puts 'Bienvenue sur Faites Les Comptes, le logiciel de compta simplifiee'
+puts ''
+puts "=== Fin de l'etape 1"
+puts ''
+
+puts "=== Lancement de l'etape 2 : demarrage du serveur"
+puts "Lorsque vous verrez les trois prochaines lignes, le serveur sera lance et vous pourrez utiliser votre navigateur prefere (Firefox, Chrome, Internet Explorer,...) a l'adresse localhost:3000."
--- start.rb
+++ start.rb
@@ -3,5 +3,8 @@
 # fichier permettant le démarrage pour la version sous windows construite par
 # le gem ocra.
 
-puts "Lancement de l'application. Merci de patienter"
+puts "Demarrage de FaitesLesComptes, logiciel open-source de comptabilite simplifiee"
+puts ''
+puts "Le lancement se fait en 2 etapes"
+puts "=== Etape 1 : lancement de ruby on rails - merci de patienter (de 30s a 1mn selon la vitesse de votre ordinateur)"
 load 'script/rails'
