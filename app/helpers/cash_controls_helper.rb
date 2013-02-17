b6f1e364c85a0d99962652fd943d537b1aae513b
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-02-17 17:44:22 +0100

    suppression de admin_cash_control_controller et des vues associées

--- app/helpers/cash_controls_helper.rb
+++ app/helpers/cash_controls_helper.rb
@@ -11,18 +11,6 @@ module CashControlsHelper
     end
   end
 
-  def admin_cash_control_submenu_helper(cash, period)
-    content_tag :span do
-      s=''
-      period.list_months('%b').each_with_index do |mois, i|
-        u =  content_tag :span do
-          link_to_unless_current(mois, admin_organism_cash_cash_controls_path(@organism, cash, "mois"=> i.to_s))
-        end
-        s += concat(u)
-      end
-      s
-    end
-
-  end
+  
 
 end
