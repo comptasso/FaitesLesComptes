e4a895991536096537e93128730e0e3f9ee8bee9
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-02-18 20:29:37 +0100

    modification du mot d accueil

--- config/initializers/bienvenue.rb
+++ config/initializers/bienvenue.rb
@@ -2,9 +2,13 @@
 
 
 # sans accent pour éviter les problèmes d'affichage sous windows
-puts ''
-puts "=== Fin de l'etape 1"
-puts ''
+if Rails.env == 'ocra'
+  puts ''
+  puts "=== Fin de l'etape 1"
+  puts ''
 
-puts "=== Lancement de l'etape 2 : demarrage du serveur"
-puts "Lorsque vous verrez les trois prochaines lignes, le serveur sera lance et vous pourrez utiliser votre navigateur prefere (Firefox, Chrome, Internet Explorer,...) a l'adresse localhost:3000."
+  puts "=== Lancement de l'etape 2 : demarrage du serveur"
+  puts "Lorsque vous verrez les trois prochaines lignes, le serveur sera lance et vous pourrez utiliser votre navigateur prefere (Firefox, Chrome, Internet Explorer,...) a l'adresse localhost:3000."
+else
+  puts 'Bienvenue sur FaitesLesComptes, le logiciel open_source de comptabilité de trésorerie'
+end
