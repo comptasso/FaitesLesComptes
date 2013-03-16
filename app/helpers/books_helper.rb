621db76803efcd408b4595d3b5b7fa58735def36
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-03-16 13:01:30 +0100

    Création d'un helper pour gérer l'affichage des liens mois dans la vue index de in_out_writings

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,28 @@
 module BooksHelper
+  
+  # Pour l'affichage d'une liste de mois dans les différentes vues avec des lignes
+  # telles que les livres, les caisses,...
+  # 
+  # Les arguments sont period (l'exercice), le MonthYear actuel, le path et un 
+  # objet qui doit être transmis au path
+  # ex submenu_mois(@period, my, 'cash_cash_lines_path', @cash)
+  # 
+  # On aura comme affichage les liens jan fév avr... chacun pointant vers l'action voulue
+  # 
+  #
+  def submenu_mois(period, opt)
+    plm = period.list_months.collect do |m|
+    content_tag :li , :class=>'active' do 
+      link_to_unless_current(m.to_short, url_for(opt.merge({:mois=>m.month, :an=>m.year})))
+    end
+    end
+    tous = content_tag :li, :class=>'active' do
+       link_to_unless_current('tous', url_for(opt.merge({:mois=>'tous'})))
+      end
+    plm.append tous
+    plm.join.html_safe
+  end
+  
+  
+  
 end
