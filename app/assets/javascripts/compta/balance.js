b3f92c2bed6c4b09a4a5097e51605fe4f158add4
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-03-17 18:10:05 +0100

    ajout d un spinner pour la création de la balance

---
+++ app/assets/javascripts/compta/balance.js
@@ -0,0 +1,35 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+jQuery(function() {
+  if ($('.compta_balances #new_compta_balance').length === 1) { // le formulaire de admin_organisms est affiché pour une création
+
+  var opts = {
+    lines: 13, // The number of lines to draw
+    length: 7, // The length of each line
+    width: 4, // The line thickness
+    radius: 26, // The radius of the inner circle
+    corners: 0.8, // Corner roundness (0..1)
+    rotate: 0, // The rotation offset
+    color: '#000', // #rgb or #rrggbb
+    speed: 1, // Rounds per second
+    trail: 60, // Afterglow percentage
+    shadow: false, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: 'auto', // Top position relative to parent in px
+    left: 'auto' // Left position relative to parent in px
+  };
+
+
+    $('#new_balance_button').click(function() {
+      var target = document.getElementById('new_compta_balance');
+      var spinner = new Spinner(opts).spin(target);
+    });
+
+  }
+})
+
