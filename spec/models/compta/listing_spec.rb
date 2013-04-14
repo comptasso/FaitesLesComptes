0ca62968ac7c05b0639ac1f0ae42490f4712e446
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-04-14 19:56:50 +0200

    complément des spec du controller compta::listing

--- spec/models/compta/listing_spec.rb
+++ spec/models/compta/listing_spec.rb
@@ -3,13 +3,25 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe Compta::Listing do
-include OrganismFixture
-     before(:each) do
-    create_organism
-    @p= @o.periods.create!(start_date:Date.today.beginning_of_year, close_date:Date.today.end_of_year)
-    @a1 = @p.accounts.find_by_number('60') 
+
+   def double_compta_line(i)
+     double(ComptaLine,
+       date:(Date.today-1),
+       narration:"Lignen° #{i}",
+       ref:'',
+       book:stub(title:'Recettes'),
+       nature_name:'nature',
+       destination_name:'dest',
+       debit:i,
+       credit:0)
+   end
 
 
+
+   before(:each) do
+    @p = mock_model(Period, start_date:Date.today.beginning_of_year, close_date:Date.today.end_of_year )
+    @a1 = mock_model(Account, :period=>@p, :period_id=>@p.id)
+    @a1.stub_chain(:compta_lines, :listing).and_return
     @listing = Compta::Listing.new 
   end
 
@@ -38,12 +50,13 @@ include OrganismFixture
       { 
         from_date:Date.today.beginning_of_month,
         to_date:Date.today.end_of_month,
-        account_id:@a1.id,
+        account_id:@a1.id
       }
     end
 
       before(:each) do
-      @l=Compta::Listing.new(valid_arguments)
+      @l = Compta::Listing.new(valid_arguments)
+      @l.stub(:account).and_return @a1
     end
 
     describe 'methods' do
@@ -52,7 +65,47 @@ include OrganismFixture
       end
 
         it 'lines returns a array of lines' do
-          @l.lines.should be_an(ActiveRecord::Relation)
+          @a1.stub_chain(:compta_lines, :listing).and_return(ar = double(Arel))
+          @l.lines.should == ar
+        end
+
+        it 'with_default_values renvoie @l après avoir mis les dates par defaut' do
+
+          listing1 =  Compta::Listing.new(:account_id=>@a1.id)
+          listing1.stub(:account).and_return @a1
+          listing1.with_default_values
+          listing1.from_date.should == Date.today.beginning_of_year
+          listing1.to_date.should == Date.today.end_of_year
+        end
+
+        it 'connait son solde de départ débit' do
+          @l.should_receive(:cumulated_debit_before).with(@l.from_date).and_return 256
+          @l.solde_debit_avant.should == 256
+        end
+
+         it 'connait son solde de départ credit' do
+          @l.should_receive(:cumulated_credit_before).with(@l.from_date).and_return -16256
+          @l.solde_credit_avant.should == -16256
+        end
+
+        it 'total debit reprend les mouvements de débit' do
+          @l.should_receive(:movement).with(@l.from_date, @l.to_date, 'debit').and_return 27.56
+          @l.total_debit.should == 27.56
+        end
+
+         it 'total credit reprend les mouvements de credit' do
+          @l.should_receive(:movement).with(@l.from_date, @l.to_date, 'credit').and_return 277777.56
+          @l.total_credit.should == 277777.56
+        end
+
+        it 'sait produire un csv' do
+          @l.stub(:solde_debit_avant).and_return 0
+          @l.stub(:solde_credit_avant).and_return 0
+          @l.stub(:total_debit).and_return 27
+          @l.stub(:total_credit).and_return 568
+          @l.stub(:lines).and_return(1.upto(45).collect {|i| double_compta_line(i)  })
+          @l.to_csv.should be_a String
+          
         end
 
   
