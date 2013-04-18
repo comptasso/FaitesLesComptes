d90c17fbe61d84d50b9085dba19014eda4e74f11
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-04-18 06:03:34 +0200

    debut de spec de PdfDetailedSheet

--- app/helpers/bank_extracts_helper.rb
+++ app/helpers/bank_extracts_helper.rb
@@ -1,28 +1,3 @@
 module BankExtractsHelper
-  def up_down(bl)
-    content_tag :td do
-     arrow_up(bl) + concat(tag("br"))+ arrow_down(bl)
-      
-    end
-  end
-
-  def arrow_up(bl)
-    unless bl.first?
-      concat(link_to image_tag('datatable/demi_sort_asc.png'),
-        up_organism_bank_account_bank_extract_bank_extract_line_path(@organism,@bank_account, @bank_extract,bl.id),
-        method: :post, remote: true)
-    else
-      concat('')
-    end
-  end
-
-  def arrow_down(bl)
-    unless bl.last?
-      concat(link_to image_tag('datatable/demi_sort_desc.png'),
-        down_organism_bank_account_bank_extract_bank_extract_line_path(@organism,@bank_account, @bank_extract,bl.id),
-        method: :post, remote: true)
-    else
-      concat('')
-    end
-  end
+  
 end
