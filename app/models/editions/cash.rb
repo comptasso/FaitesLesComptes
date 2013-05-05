08b56af080ab178249556cf4f6ad56b50f4df6bd
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-05-05 18:38:26 +0200

    fait les spec de Editions::Cash

--- app/models/editions/cash.rb
+++ app/models/editions/cash.rb
@@ -4,7 +4,7 @@ module Editions
 
   # Classe destinée à imprimer un livre ou un extrait de livre en format pdf
   #
-  # Cette classe hérite de PdfDocument::Default et surcharge fetch_lines
+  # Cette classe hérite de Editions::Book et surcharge fetch_lines
   class Cash < Editions::Book
 
 
@@ -14,8 +14,8 @@ module Editions
        set_columns ['writings.date AS w_date', 'writings.ref AS w_ref',
         'writings.narration AS w_narration', 'destination_id',
         'nature_id', 'credit', 'debit']
-       set_columns_methods ['writing.date', 'writing.ref', 'writing.narration',
-        'destination_id.name', 'nature_id.name', 'credit', 'debit']
+       set_columns_methods ['w_date', 'w_ref', 'w_narration',
+        'destination.name', 'nature.name', 'credit', 'debit']
        
        set_columns_widths([12, 12, 28,12,12,12,12])
        set_columns_to_totalize [5,6]
---
+++ spec/models/editions/cash_spec.rb
@@ -0,0 +1,69 @@
+# coding: utf-8
+
+require'spec_helper'
+
+describe Editions::Cash do
+
+  def line(date, debit, credit)
+      double(ComptaLine, writing_id:1, w_ref:'',w_narration:'Une compta line',
+        destination:stub(:name=>'La destination'),
+        nature:stub(:name=>'La nature'),
+        debit:debit,
+        credit:credit,
+        w_date:date,
+        w_mode: 'Chèque',
+        writing:stub(payment_mode:'Chèque'),
+        support:'Ma banque',
+        locked?:true)
+    end
+
+  before(:each) do
+     @cash = mock_model(Cash)
+     @period = stub(Period, organism:stub(:title=>'L\'organisme'), :exercice=>'Exercice en cours')
+     @extract = stub(Extract::InOut, :book=>@cash,
+       begin_date:Date.today.beginning_of_year,
+       end_date:Date.today.end_of_year,
+       'provisoire?'=>true,
+     title:'Le livre',
+     subtitle:'Le sous titre',
+     titles:%w(un deux trois quatre cinq),
+     lines:(50.times.collect {line(Date.today, 1.25, 0.3)}))
+
+
+
+  end
+
+ it 'peut se créer' do
+   Editions::Cash.new(@period, @extract)
+ end
+
+
+# FIXME prepare_line semble être rendu appelé 150 fois, ce qui laisse penser
+# que perpare_line est appelé à chaque page pour l'ensemble des 50 lignes
+# et non simplement pour les 22 lignes de la page
+  it 'et peut alors rendre un pdf' do
+    @extract.stub(:instance_eval).and_return(@ar = double(Arel, :count=>50))
+    @ar.stub_chain(:select, :offset, :limit).and_return(@extract.lines)
+    @eb = Editions::Cash.new(@period, @extract)
+    @eb.should_receive(:prepare_line).at_least(50).times.and_return %w(un doux)
+    @eb.render
+  end
+
+
+  # FIXME ce ne devrait pas être Date.today mais I18n::l Date.today
+  # voir la méthode de Editions::Book.
+  it 'prepare_line' do
+    Writing.stub(:find_by_id).and_return(double(Writing, support:'CrédiX', :payment_mode=>'Chèque' ))
+    @eb = Editions::Cash.new(@period, @extract)
+    @eb.prepare_line(line(Date.today, 1.25, 0.3)).should == [I18n.l(Date.today, format:'%d/%m/%Y'),
+        "", 
+        "Une compta line",
+        "La destination",
+        "La nature",
+        0.3,
+        1.25]
+
+  end
+
+
+end
