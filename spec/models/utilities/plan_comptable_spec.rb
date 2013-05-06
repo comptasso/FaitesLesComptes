de4cd5202f01db8ef1d087abd8d5ed2fee6f6d41
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-05-06 06:39:26 +0200

    complété la spec du plan comptable avec le cas ou YAML.load_file génère une erreur

--- spec/models/utilities/plan_comptable_spec.rb
+++ spec/models/utilities/plan_comptable_spec.rb
@@ -7,13 +7,13 @@ describe Utilities::PlanComptable do
 
   before(:each) do 
      
-     @p = Period.new(start_date:Date.today.beginning_of_month, close_date:Date.today.end_of_month)
-     @p.organism_id = 1
-     @p.stub(:should_not_have_more_than_two_open_periods).and_return(true)
-     @p.stub(:create_plan) # car create_plan est appelé par un after_create
-     @p.stub(:create_bank_and_cash_accounts) # inutile de tester ce point ici
-     @p.stub(:load_natures)
-     @p.save!
+    @p = Period.new(start_date:Date.today.beginning_of_month, close_date:Date.today.end_of_month)
+    @p.organism_id = 1
+    @p.stub(:should_not_have_more_than_two_open_periods).and_return(true)
+    @p.stub(:create_plan) # car create_plan est appelé par un after_create
+    @p.stub(:create_bank_and_cash_accounts) # inutile de tester ce point ici
+    @p.stub(:load_natures)
+    @p.save!
   end
 
   it 'se crée avec un exercice et un statut d organisme' do
@@ -22,16 +22,24 @@ describe Utilities::PlanComptable do
 
   describe 'self.create_accounts' do
 
-  it 'demande à period de créer les comptes lus dans le fichier' do
-    Utilities::PlanComptable.create_accounts(@p, 'Association').should == 87
-    @p.should have(87).accounts
-  end
+    it 'demande à period de créer les comptes lus dans le fichier' do
+      Utilities::PlanComptable.create_accounts(@p, 'Association').should == 87
+      @p.should have(87).accounts
+    end
 
-  it 'si p a déja des comptes ne les écrase pas' do
-    @p.accounts.create!(number:101, title:'Fonds associatifs', :period_id=>@p.id)
-    Utilities::PlanComptable.create_accounts(@p, 'Association').should == 86
-    @p.accounts(true).should have(87).accounts
-  end
+    it 'si p a déja des comptes ne les écrase pas' do
+      @p.accounts.create!(number:101, title:'Fonds associatifs', :period_id=>@p.id)
+      Utilities::PlanComptable.create_accounts(@p, 'Association').should == 86
+      @p.accounts(true).should have(87).accounts
+    end
+
+    context 'en cas d erreur lors de la lecture du fichier' do
+
+      it 'en cas d erreur sur la lecture : retourn 0' do
+        Utilities::PlanComptable.create_accounts(@p, 'Inconnu').should == 0
+      end
+
+    end
 
   end
 
