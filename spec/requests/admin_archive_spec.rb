d4139d8e31d5b1de92302fc1c429134df68af362
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-05-07 07:10:18 +0200

    spec request archive tolère maintenant un changement de seconde dans le test (concerne le nom du fichier créé

--- spec/requests/admin_archive_spec.rb
+++ spec/requests/admin_archive_spec.rb
@@ -38,7 +38,7 @@ describe 'resquest admin archive' do
       # pour éviter d'avoir des erreurs liées à un changement de seconde
       # pendant le test, on isole le dernier chiffre et on crée une expression
       # régulière
-      filename = name[0,name.length-1]+'[0-9]'+'.sqlite3'
+      filename = name[0,name.length-2]+'[0-5][0-9]'+'.sqlite3'
       cd = page.response_headers['Content-Disposition']
       cd[/attachment; filename=(.*)/]
       $1.should match filename # contrôle du titre du fichier
