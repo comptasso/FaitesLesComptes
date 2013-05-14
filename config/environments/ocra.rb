53417b9b5f9259506ea27901e9c1849b4c29002d
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-05-14 07:29:40 +0200

    remis les logs de ocra à leur place normale

--- config/environments/ocra.rb
+++ config/environments/ocra.rb
@@ -35,9 +35,7 @@ Faitesvoscomptes::Application.configure do
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
-  log_file = File.expand_path('../logs/flc.log', ENV['OCRA_EXECUTABLE'])
-  config.logger = ActiveSupport::BufferedLogger.new(log_file)
-
+ 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
 
