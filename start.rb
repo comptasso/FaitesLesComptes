6e595aac94e6c142347dd1f54dbc20572731541c
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-05-16 07:13:54 +0200

    e reviens en arrière sur start.rb car utiliser #{VERSION} pose problème

--- start.rb
+++ start.rb
@@ -3,9 +3,7 @@
 # fichier permettant le démarrage pour la version sous windows construite par
 # le gem ocra.
 
-require 'config/initializers/all_constants'
-
-puts "Demarrage de FaitesLesComptes, verssion #{VERSION}, logiciel open-source de comptabilite de trésorerie et de comptabilité simplifiee"
+puts "Demarrage de FaitesLesComptes, version 0.7, logiciel open-source de comptabilite de trésorerie et de comptabilité simplifiee"
 puts ''
 puts "=== lancement de ruby on rails - merci de patienter (de 30s a 1mn selon la vitesse de votre ordinateur)"
 load 'script/rails'
