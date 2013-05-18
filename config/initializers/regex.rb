5406e88916ae8a0b93c977ad5b22494d2cf5e3ce
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-05-18 19:58:41 +0200

    commentaires du fichier regex.rb

--- config/initializers/regex.rb
+++ config/initializers/regex.rb
@@ -1,7 +1,19 @@
 # des regex pour faciliter les validations de format avec des caractères accentués
 
-ALNUM = '[a-zA-Z0-9]|[\u00e0-\u00ff]'
-ALNUMEND = '[a-zA-Z0-9]|[\u00e0-\u00ff]|[\.]'
-WORDCHARS =  '[a-zA-Z0-9]|[\u00e0-\u00ff]|\s|[\u0153@&_\-\'\.,]'
-WORD = "((#{ALNUM})((#{WORDCHARS})*(#{ALNUMEND}))?)"
-NAME_REGEX = /^#{WORD}$/
+
+# Définition des constantes pour les REGEX
+#
+# NAME_REGEX doit commercer par un chiffre, une lettre minuscule ou majuscule ou encore
+# minuscule accentuée.
+#
+# Ce premier caractère est suivi par autant de caractères du même type mais ce peut être
+# aussi le signe @ ou & ou un point ou une virgule ou une apostrophe
+#
+# Le dernier caractère de la chaîne ne peut être qu'un ALNUM ou un point pour marquer la fin
+# de la phrase. Par exemple pour un commentaire.
+#
+ALNUM = '[a-zA-Z0-9]|[\u00e0-\u00ff]'    # chiffre, lettre et caractères accentués
+ALNUMEND = '[a-zA-Z0-9]|[\u00e0-\u00ff]|[\.]' # les mêmes plus le point final
+WORDCHARS =  '[a-zA-Z0-9]|[\u00e0-\u00ff]|\s|[\u0153@&_\-\'\.,]' # les mêmes plus les espaces, @&-', et .
+WORD = "((#{ALNUM})((#{WORDCHARS})*(#{ALNUMEND}))?)"  # on regroupe le tout
+NAME_REGEX = /^#{WORD}$/ # pour obtenir le regex
