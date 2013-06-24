a5b2fa9e9aa6372d079df9ba4ce7de23ae28b629
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-06-24 07:13:54 +0200

    revu RestoreController suite à Devise

--- spec/controllers/admin/restores_controller_spec.rb
+++ spec/controllers/admin/restores_controller_spec.rb
@@ -8,20 +8,21 @@ end
 
 class RestoreError < StandardError; end  
 
-describe Admin::RestoresController do 
+describe Admin::RestoresController do
+  include SpecControllerHelper
   include ActionDispatch::TestProcess
 
   let(:cu) {mock_model(User)}
   let(:cu2) {mock_model(User)}
 
-  def valid_session
-    {user:cu.id}
+  before(:each) do
+    sign_in(cu)
   end
-
+ 
   describe 'GET new' do
 
     it 'render new' do
-      get :new, {}, valid_session
+      get :new
       response.should render_template('new')
     end
 
@@ -49,7 +50,7 @@ describe Admin::RestoresController do
 
       it 'redirige vers la liste des fichiers'  do
         
-        post :create, {:file_upload=>@file, database_name:'test2'}, valid_session
+        post :create, {:file_upload=>@file, database_name:'test2'}
         flash[:notice].should == "Le fichier a été chargé et peut servir de base de données"
         response.should redirect_to admin_rooms_url
       end
@@ -57,7 +58,7 @@ describe Admin::RestoresController do
       it 'met à jour le nom de database_name pour l organisme' do
         Organism.should_receive(:first).and_return(@o=mock_model(Organism))
         @o.should_receive(:update_attribute).with(:database_name, 'test2')
-        post :create, {:file_upload=>@file, database_name:'test2'}, valid_session
+        post :create, {:file_upload=>@file, database_name:'test2'}
       end
 
     end
@@ -72,7 +73,7 @@ describe Admin::RestoresController do
 
       it 'vérifie que l adapter est sqlite3' do
         @controller.stub(:abc).and_return({:adapter=>'passqlite3'})
-        post :create, {:file_upload=>@file, database_name:'test'}, valid_session
+        post :create, {:file_upload=>@file, database_name:'test'}
         flash[:alert].should == "La restauration de bases n'est possible que pour les bases sqlite3"
         response.should render_template 'new'
       end
@@ -80,8 +81,8 @@ describe Admin::RestoresController do
       it 'vérifie que le nom de base n est pas pris par un autre user' do
         Room.should_receive(:find_by_database_name).with('test').and_return(@ro)
         @ro.should_not be_nil
-        @ro.user.should_not == cu
-        post :create, {:file_upload=>@file, database_name:'test'}, valid_session
+        @ro.user.should_not == cu 
+        post :create, {:file_upload=>@file, database_name:'test'}
         flash[:alert].should == 'Ce nom de base est déjà pris et ne vous appartient pas'
         response.should render_template 'new'
       end
@@ -89,21 +90,21 @@ describe Admin::RestoresController do
 
       it 'vérifie le format de fichier'   do
         cu.stub_chain(:rooms, :new).and_return(@r = mock_model(Room, 'save!'=>true))
-        post :create, {:file_upload=>fixture_file_upload('spec/fixtures/files/test.biz', 'application/octet-stream'), database_name:'test'}, valid_session
+        post :create, {:file_upload=>fixture_file_upload('spec/fixtures/files/test.biz', 'application/octet-stream'), database_name:'test'}
         flash[:alert].should == "L'extension .biz du fichier ne correspond pas aux bases gérées par l'application : .sqlite3"
       end
       
       it 'vérifie le format de database'  do
         cu.stub_chain(:rooms, :new).and_return @ro
         @ro.should_receive(:valid?).and_return(false)
-        post :create, {:file_upload=>@file, database_name:'2test2'}, valid_session
+        post :create, {:file_upload=>@file, database_name:'2test2'}
         flash[:alert].should == 'Nom de base non valide : impossible de créer la base'
       end
 
       it 'vérifie la base' do
         cu.stub_chain(:rooms, :new).and_return @ro
         @ro.stub(:check_db).and_return false
-        post :create, {:file_upload=>@file, database_name:'test'}, valid_session
+        post :create, {:file_upload=>@file, database_name:'test'}
         flash[:alert].should == 'Le contrôle du fichier par SQlite renvoie une erreur'
       end
 
@@ -121,13 +122,13 @@ describe Admin::RestoresController do
 
         it 'redirige vers admin_rooms' do 
           @ro.stub(:relative_version).and_return(:same_migration)
-          post :create, {:file_upload=>@file, database_name:'test'}, valid_session
+          post :create, {:file_upload=>@file, database_name:'test'}
           response.should redirect_to admin_rooms_url
         end
       
         it 'si la version est same_migration'  do
           @ro.stub(:relative_version).and_return(:same_migration)
-          post :create, {:file_upload=>@file, database_name:'test'}, valid_session
+          post :create, {:file_upload=>@file, database_name:'test'}
           flash[:notice].should == "Le fichier a été chargé et peut servir de base de données"
         end
       
