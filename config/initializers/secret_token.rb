2b66f7f4932ed0486f377b97034d41ce00847f9c
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-07-07 14:24:32 +0200

    correction de la faille de rails sur le secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,27 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Faitesvoscomptes::Application.config.secret_token = '99376ad1fd8ef56bef36f7f98501d2b21df1b0f80482a3ea86287e5b15f1f8aef76315d6d28b9f68eaa393a02527e3bd99dbf49f8bdb9c2f2eda63dae2202d57'
+
+# Be sure to restart your server when you modify this file.
+
+require 'securerandom'
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+
+def find_secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist? token_file
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token of 64 random hexadecimal characters and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Faitesvoscomptes::Application.config.secret_token = find_secure_token
