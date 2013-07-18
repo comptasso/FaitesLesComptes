8ab4dfd466c0e3c521fe4184ae0358148afe2d4c
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-07-18 07:39:51 +0200

    modification du default from

--- app/mailers/user_inscription.rb
+++ app/mailers/user_inscription.rb
@@ -1,12 +1,11 @@
 class UserInscription < ActionMailer::Base
-  default :from=>'UserObserver'
+  default :from=>'expert@faiteslescomptes.fr'
 
   def new_user_advice(user)
     @user = user
     mail(
       :to=>'expert@faiteslescomptes.fr',
-      :subject=>'ouverture d\'un nouveau compte',
-      :from=>'UserObserver')
+      :subject=>'ouverture d\'un nouveau compte')
   end
 
 end
