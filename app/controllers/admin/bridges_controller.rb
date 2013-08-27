9b8ece5a860b7716dfb5c94f8453fdd1f9fde048
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-08-27 13:12:52 +0200

    introduction du check_nature_name et du flash dans le controller bridge

--- app/controllers/admin/bridges_controller.rb
+++ app/controllers/admin/bridges_controller.rb
@@ -11,7 +11,12 @@ class Admin::BridgesController < Admin::ApplicationController
     @bridge = @organism.bridge
     if @bridge.update_attributes(params[:bridge])
       flash[:notice] = 'Les paramètres ont été modifiés'
-      redirect_to admin_organism_bridge_url(@organism)
+      unless @bridge.check_nature_name
+      flash[:alert] = "Attention, un exercice ouvert ne dispose pas de ce nom de nature. <br/>
+L'enregistrement des paiements des adhérents sera alors impossible pour ce ou ces exercices.<br/>
+        Nous vous conseillons d'harmoniser le nom de la nature pour tous les exercices ouverts".html_safe
+      end
+       redirect_to admin_organism_bridge_url(@organism)
     else
       flash[:alert] = 'Impossible d\'enregistrer les paramètres'
       render action: 'edit'
