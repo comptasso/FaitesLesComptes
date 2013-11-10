0b5c09b7fd2b9a748373fca362b348148e6c60ed
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-11-10 09:12:47 +0100

    changement du n° de version

--- config/initializers/all_constants.rb
+++ config/initializers/all_constants.rb
@@ -59,4 +59,5 @@ MAX_COMMENT_LENGTH = 300
 # VERSION = '1.0.2RC' # correction bug - la liste des organismes n'était pas mise à jour après une suppression 
 # VERSION = '1.1.0RC' # suppression de la table bank_extract_lines_lines et refonte du pointage des relevés 
 # FLCVERSION = '1.1.1RC' # revu le javascript du pointage et suppression de facebox.js inutilisé 
-FLCVERSION = '1.2.0RC' # folio et rubrik deviennent des éléments persistants. 
+# FLCVERSION = '1.2.0RC' # folio et rubrik deviennent des éléments persistants. 
+FLCVERSION = '1.2.1RC' # Mise en place d'une vue des écritures non pointées et travail sur le solde instantané 
