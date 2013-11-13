78f328f539950209cb07b07d433bff0f7327c832
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-11-13 07:16:01 +0100

    revu la fonction permettant à datatable d identifier le type comma-numeric pour permettre le classement lorsque des nombres ont un séparateur de milliers

--- app/assets/javascripts/zddons.dataTable.js
+++ app/assets/javascripts/zddons.dataTable.js
@@ -123,47 +123,28 @@ $.extend($.fn.dataTableExt.oPagination, {
 jQuery.fn.dataTableExt.aTypes.unshift(
     function ( sData )
     {
-        var sValidChars = "0123456789-,";
-        var Char;
-        var bDecimal = false;
-
-        /* Check the numeric part */
-        for ( i=0 ; i<sData.length ; i++ )
-        {
-            Char = sData.charAt(i);
-            if (sValidChars.indexOf(Char) == -1)
-            {
-                return null;
-            }
-
-            /* Only allowed one decimal place... */
-            if ( Char == "," )
-            {
-                if ( bDecimal )
-                {
-                    return null;
-                }
-                bDecimal = true;
-            }
+        var regex = /-?[1-9](?:\d{0,2})(?:\s\d{3})*(?:,\d\d)|-?0,\d\d|-/;
+        if (sData.match(regex)) {
+          return 'numeric-comma';
+        } else {
+          return null;
         }
-
-        return 'numeric-comma';
     }
 );
 
 
 // fonctions de tri pour des nombres avec une virgule comme séparateur décimal
     jQuery.fn.dataTableExt.oSort['numeric-comma-asc']  = function(a,b) {
-    var x = (a == "-") ? 0 : a.replace( /,/, "." );
-    var y = (b == "-") ? 0 : b.replace( /,/, "." );
+    var x = (a === "-") ? 0 : a.replace( ',', "." ).replace(' ', '');
+    var y = (b === "-") ? 0 : b.replace( ',', "." ).replace(' ', '');;
     x = parseFloat( x );
     y = parseFloat( y );
     return ((x < y) ? -1 : ((x > y) ?  1 : 0));
 };
 
 jQuery.fn.dataTableExt.oSort['numeric-comma-desc'] = function(a,b) {
-    var x = (a == "-") ? 0 : a.replace( /,/, "." );
-    var y = (b == "-") ? 0 : b.replace( /,/, "." );
+    var x = (a === "-") ? 0 : a.replace( ',', "." ).replace(' ', '');;
+    var y = (b === "-") ? 0 : b.replace( ',', "." ).replace(' ', '');;
     x = parseFloat( x );
     y = parseFloat( y );
     return ((x < y) ?  1 : ((x > y) ? -1 : 0));
