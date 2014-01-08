b900ba861a8949b0d3e38ca70e10524c9d06b58d
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2014-01-08 20:43:58 +0100

    création migration pour modification de Nature

---
+++ db/migrate/20140108193729_add_column_book_id_to_natures.rb
@@ -0,0 +1,21 @@
+class AddColumnBookIdToNatures < ActiveRecord::Migration
+#  def up
+#    add_column :natures, :book_id, :integer
+#    
+#    Nature.all.each {|n| fill_book_id(n)}
+#    
+#    remove_column :natures, :income_outcome
+#  end
+#  
+#  def down
+#    remove_column :natures, :book_id
+#  end
+#  
+#  protected
+#  
+#  # si la nature est de type recettes, on 
+#  def fill_book_id(nature)
+#    
+#  end
+  
+end
