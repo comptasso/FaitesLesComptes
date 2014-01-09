fe4567276555e51ca4f6dddf8110c8fd0cc48069
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2014-01-09 05:39:20 +0100

    remplacement du champ income_outcome par un book_id pour Nature

--- db/migrate/20140108193729_add_column_book_id_to_natures.rb
+++ db/migrate/20140108193729_add_column_book_id_to_natures.rb
@@ -1,21 +1,32 @@
+# remplace income_outcome de Nature par un book_id et met 
+# à jour les natures existantes
 class AddColumnBookIdToNatures < ActiveRecord::Migration
-#  def up
-#    add_column :natures, :book_id, :integer
-#    
-#    Nature.all.each {|n| fill_book_id(n)}
-#    
-#    remove_column :natures, :income_outcome
-#  end
-#  
-#  def down
-#    remove_column :natures, :book_id
-#  end
-#  
-#  protected
-#  
-#  # si la nature est de type recettes, on 
-#  def fill_book_id(nature)
-#    
-#  end
+  def up
+    add_column :natures, :book_id, :integer
+    Nature.reset_column_information
+    Nature.all.each {|n| fill_book_id(n)}
+    remove_column :natures, :income_outcome
+  end
+  
+  def down
+    add_column :natures, :income_outcome, :boolean, default:false
+    Nature.reset_column_information
+    Nature.all.each {|n| fill_income_outcome(n)}
+    remove_column :natures, :book_id
+  end
+  
+  protected
+  
+  # si la nature est de type recettes, on met comme référence le premier livre de recettes
+  # sinon, le premier livre de dépenses
+  def fill_book_id(nature)
+    bid = nature.income_outcome ? IncomeBook.first.id : OutcomeBook.first.id 
+    nature.update_attribute(:book_id, bid)
+  end
+  
+  def fill_income_outcome(nature)
+    nio = nature.book.type == 'IncomeBook' ? true : false 
+    nature.update_attribute(:income_outcome, nio)
+  end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140105170944) do
+ActiveRecord::Schema.define(:version => 20140108193729) do
 
   create_table "accounts", :force => true do |t|
     t.string   "number"
@@ -240,12 +240,12 @@ ActiveRecord::Schema.define(:version => 20140105170944) do
   create_table "natures", :force => true do |t|
     t.string   "name"
     t.text     "comment"
-    t.datetime "created_at",                        :null => false
-    t.datetime "updated_at",                        :null => false
-    t.boolean  "income_outcome", :default => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.integer  "period_id"
     t.integer  "account_id"
     t.integer  "position"
+    t.integer  "book_id"
   end
 
   create_table "nomenclatures", :force => true do |t|
