56a1cec7e999fb46d4ddc282707eee02e85e044c
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2014-02-12 06:50:38 +0100

    debut de mise en place de l accélération pour les graphiques de books

--- app/models/book.rb
+++ app/models/book.rb
@@ -65,6 +65,60 @@ class Book < ActiveRecord::Base
     super
   end
 
+  
+  #protected
+  
+    # mise en place des fonctions qui permettent de construire les graphiques avec 
+  # très peu d'appel à la base de données
+  # récupère les soldes pour une caisse pour un exercice
+ #
+ # Renvoie un hash selon le format suivant 
+ # {"09-2013"=>"-24.00", "01-2013"=>"-75.00", "08-2013"=>"-50.00"}
+ #
+ # Les mois où il n'y a pas de valeur ne renvoient rien.
+ # Il faut donc ensuite faire un mapping ce qui est fait par la méthode
+ # map_query_months(period)
+ #
+ def query_monthly_datas(period)
+   
+   
+sql = <<-hdoc
+ SELECT 
+     to_char(writings.date, 'MM-YYYY') AS mony,
+   
+     SUM(compta_lines.credit) - SUM(compta_lines.debit) AS valeur 
+ FROM 
+     
+     writings, 
+     compta_lines
+ WHERE
+    
+   writings.book_id = #{id} AND 
+writings.date >= '#{period.start_date}' AND 
+writings.date <= '#{period.close_date}' AND 
+     compta_lines.writing_id = writings.id
+     
+ GROUP BY mony
+hdoc
+
+   res = VirtualBook.connection.execute( sql.gsub("\n", ''))
+   h = Hash.new('0')
+   res.each {|r| h[r['mony']]= r["valeur"] }
+   h
+   
+   end
+ 
+ 
+  # A partir de query_monthly_datas, construit les valeurs mensuelles
+ # sans trou
+# def monthly_datas_for_chart(months)
+#   # trouve l'exercice correspondant 
+#   p = organism.find_period(months.to_a.last.beginning_of_month)
+#   h = query_monthly_datas(p)
+#   datas  = months.collect { |my| h[my.to_s]}
+#   datas
+# end
+# 
  
 
 end
