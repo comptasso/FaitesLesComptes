58f97dd20a60bcd463f5b4d54629af509f7a01ed
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2014-05-31 18:45:14 +0200

    commentaire du controller users devenu inutile

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,87 +2,87 @@
 
 # TODO probablement à supprimer puisque le user est maintenant géré par Devise
 
-class Admin::UsersController < Admin::ApplicationController
-
-  skip_before_filter :find_organism, :current_period
-  skip_before_filter :log_in?
-  before_filter :use_main_connection
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all 
- 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @users }
-    end
-  end
-
-  # GET /users/1
-  # GET /users/1.json
-  def show
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @user }
-    end
-  end
-
-  # GET /users/new
-  # GET /users/new.json
-  def new
-    @user = User.new(name:params[:name])
-    flash[:alert] = nil
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user }
-    end
-  end
-
-  # GET /users/1/edit
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(params[:user])
-    if @user.save
-      session[:user] = @user.id
-      redirect_to new_admin_room_url, notice: "L'utilisateur a été crée. Il est possible maitenant de créer un organisme (formulaire ci-dessous)\n
-      ou d'en importer (icône disquette ci-dessus)"
-    else
-      render action: "new"
-    end
-  end
-
-  # PUT /users/1
-  # PUT /users/1.json
-  def update
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :ok }
-    end
-  end
-end
+#class Admin::UsersController < Admin::ApplicationController
+#
+#  skip_before_filter :find_organism, :current_period
+#  skip_before_filter :log_in?
+#  before_filter :use_main_connection
+#  # GET /users
+#  # GET /users.json
+#  def index
+#    @users = User.all 
+# 
+#    respond_to do |format|
+#      format.html # index.html.erb
+#      format.json { render json: @users }
+#    end
+#  end
+#
+#  # GET /users/1
+#  # GET /users/1.json
+#  def show
+#    @user = User.find(params[:id])
+#
+#    respond_to do |format|
+#      format.html # show.html.erb
+#      format.json { render json: @user }
+#    end
+#  end
+#
+#  # GET /users/new
+#  # GET /users/new.json
+#  def new
+#    @user = User.new(name:params[:name])
+#    flash[:alert] = nil
+#    respond_to do |format|
+#      format.html # new.html.erb
+#      format.json { render json: @user }
+#    end
+#  end
+#
+#  # GET /users/1/edit
+#  def edit
+#    @user = User.find(params[:id])
+#  end
+#
+#  # POST /users
+#  # POST /users.json
+#  def create
+#    @user = User.new(params[:user])
+#    if @user.save
+#      session[:user] = @user.id
+#      redirect_to new_admin_room_url, notice: "L'utilisateur a été crée. Il est possible maitenant de créer un organisme (formulaire ci-dessous)\n
+#      ou d'en importer (icône disquette ci-dessus)#"
+#    else
+#      render action: "new"
+#    end
+#  end
+#
+#  # PUT /users/1
+#  # PUT /users/1.json
+#  def update
+#    @user = User.find(params[:id])
+#
+#    respond_to do |format|
+#      if @user.update_attributes(params[:user])
+#        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+#        format.json { head :ok }
+#      else
+#        format.html { render action: "edit" }
+#        format.json { render json: @user.errors, status: :unprocessable_entity }
+#      end
+#    end
+#  end
+#
+#  # DELETE /users/1
+#  # DELETE /users/1.json
+#  def destroy
+#    @user = User.find(params[:id])
+#    @user.destroy
+#
+#    respond_to do |format|
+#      format.html { redirect_to users_url }
+#      format.json { head :ok }
+#    end
+#  end
+#end
