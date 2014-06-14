8323f50cf09c7cdb1b08c33001bf95b4e7454986
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2014-06-14 14:58:39 +0200

    changement des délais d affichage et de num de version

--- app/assets/javascripts/admin/period.js
+++ app/assets/javascripts/admin/period.js
@@ -8,9 +8,9 @@
       $('.inner-champ').block({ message: '<h4 id="message-d-attente">Juste un instant...</h4>' });
       setTimeout(function() {
         $('#message-d-attente').text('Création du plan de comptes...');
-      }, 1500);
+      }, 2000);
       setTimeout(function() {
         $('#message-d-attente').text('Préparation des modèles de rapport...');
-      }, 3000);                               
+      }, 4000);                               
     });   
 });
