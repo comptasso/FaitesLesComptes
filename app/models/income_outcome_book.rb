5cac035ebc95280703f9d7c9890601d0fa601ce9
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2014-06-27 07:40:12 +0200

    methode sql en test pour l affichage des writings

--- app/models/income_outcome_book.rb
+++ app/models/income_outcome_book.rb
@@ -33,6 +33,40 @@ class IncomeOutcomeBook < Book
   def extract_lines(from_date, to_date)
     in_out_lines.where('writings.date >= ? AND writings.date <= ?', from_date, to_date).order('writings.date')
   end
+  
+  
+  def test_extract_lines(from_date, to_date)
+    sql = <<-hdoc
+      SELECT writings.book_id, writings.id, writings.date, writings.ref, writings.narration,
+      cls.nname, cls.dname, cls.debit, cls.credit,
+      support.pay_mode, acc_number, acc_title, bel_id
+      FROM
+      writings, 
+      (SELECT compta_lines.writing_id as wid, natures.name AS nname,
+       destinations.name AS dname, 
+      compta_lines.debit AS debit,
+       compta_lines.credit AS credit
+      FROM compta_lines 
+      LEFT JOIN natures ON (natures.id = compta_lines.nature_id)
+      LEFT JOIN destinations ON (destinations.id = compta_lines.destination_id)
+      WHERE compta_lines.nature_id IS NOT NULL) as cls, 
+      (SELECT compta_lines.writing_id AS wid, payment_mode AS pay_mode, 
+      accounts.number AS acc_number, accounts.title AS acc_title, 
+      bank_extract_lines.id AS bel_id
+
+      FROM compta_lines 
+      LEFT JOIN accounts ON accounts.id = compta_lines.account_id
+      LEFT JOIN bank_extract_lines ON bank_extract_lines.compta_line_id = compta_lines.id
+      WHERE nature_id IS NULL) as support
+      WHERE 
+      writings.book_id = '#{id}' AND
+      cls.wid = writings.id AND support.wid = writings.id AND 
+      writings.date >= '#{from_date}' AND 
+      writings.date <= '#{to_date}'  
+    hdoc
+    res = IncomeOutcomeBook.connection.execute( sql.gsub("\n", ''))
+    res
+  end
 
   # surchargée car l'affichage des montants et des lignes dans la vue ne doit prendre en
   # compte que les lignes qui ont une nature et être limité à l'exercice.
