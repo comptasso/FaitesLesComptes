bb9abe16ec342dae884ea2ad5f386257f1677abc
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2014-07-30 10:18:59 +0200

    installation de devise_async

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-    :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable, :async
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
