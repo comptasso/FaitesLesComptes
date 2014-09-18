3c6fe4eec8658e2fe132b1913d580a6ac8472201
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2014-09-18 06:38:09 +0200

    ajout d un test sur Editions::AnalyticalBalance
    l idée est dêtre sur que toutes les lignes sont des TableLines

--- spec/models/editions/analytical_balance_spec.rb
+++ spec/models/editions/analytical_balance_spec.rb
@@ -12,16 +12,30 @@ describe Editions::AnalyticalBalance do
   
   subject {Editions::AnalyticalBalance.new(@cab)}
   
+  describe 'les lignes' do
+    
+  before(:each) do
+    create_in_out_writing
+  end
+  
+  after(:each) {Writing.delete_all}
+  
+  it 'ses lignes ne sont que des PdfDocument::TableLine' do
+    subject.nb_pages.should == 1
+    leslignes = subject.fetch_lines(1)
+    leslignes.size.should == 5
+    leslignes.each {|l| puts l.inspect}
+    leslignes.each {|l| l.should be_an_instance_of(PdfDocument::TableLine)}
+  end
+  
+  end
   
   describe 'render' do
-    
+     
     it 'ne renvoie pas d erreur' do
       expect {subject.render}.not_to raise_error 
     end
     
   end
-  
-  
-  
   
 end
