22c421e210d112aa105c353959ed736b64c12a09
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2014-10-24 19:20:00 +0200

    fichier pdflc::flc_table pour la refonte des listings et grand livre

--- app/models/pdflc/flc_table.rb
+++ app/models/pdflc/flc_table.rb
@@ -18,15 +18,16 @@ module Pdflc
   # 
   class FlcTable
     
-    attr_reader :columns_to_totalize, :fields
+    attr_reader :columns_to_totalize, :fields, :date_fields
     
-    def initialize(query,  page, nb_lines_per_page,
-        columns_to_totalize = [], fields = [])
-      @query = query
-      @page = page
+    def initialize(arel, page_number, nb_lines_per_page, fields,
+        columns_to_totalize = [], date_fields = [])
+      @arel = arel
+      @page_number = page_number
       @nb_lines_per_page = nb_lines_per_page
-      @columns_to_totalize = columns_to_totalize
       @fields = fields
+      @columns_to_totalize = columns_to_totalize
+      @date_fields = date_fields
     end
     
     def lines
@@ -36,12 +37,26 @@ module Pdflc
     # les lignes préparées sont la transformation des champs à additionner 
     # en champ décimaux
     def prepared_lines
-      
+      # appelle les méthodes adéquates pour chacun des éléments de la lignes
+    # A surcharger lorsqu'on veut faire un traitement de la ligne
+    # Par défaut applique number_with_precision à toutes les valeurs numériques
+    
+      @prepared_lines ||= lines.collect do |l|
+        @fields.collect.with_index do |f, i| 
+          v = l.send(f)
+          v = french_format(v) if i.in?(columns_to_totalize)
+          v = I18n.l(v.to_date, format:'%d-%m-%Y') if i.in?(date_fields)
+          v
+        end
+      end
+    
     end
     
     # renvoie un array de Decimal, correspondant aux champs qui sont à totaliser
     def totals
-      
+      columns_to_totalize.collect.with_index do |ctt, i|
+        lines.sum(&fields[i])
+      end
     end
     
     protected
@@ -49,8 +64,13 @@ module Pdflc
     # ici on exécute la query avec le nombre de lignes demandées et l'offset
     # renvoyant à la page souhaitée
     def fetch_lines
-      # query.offset.limit
-      # puis préparation des lignes
+      limit = @nb_lines_per_page
+      offset = (@page_number - 1)*@nb_lines_per_page
+      @arel.offset(offset).limit(limit)
+    end
+    
+    def french_format(num)
+      ActionController::Base.helpers.number_with_precision(num, :precision=>2)
     end
     
   end
