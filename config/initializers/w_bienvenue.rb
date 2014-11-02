835cbf80f7e90ee4b478329794bcd791e5ece203
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2014-11-02 09:51:29 +0100

    historique des versions dans son fichier spécifique

---
+++ config/initializers/w_bienvenue.rb
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+# Ce fichier fait partie des initializers et le message est est donc 
+# affiché dans la console lors du lancement.
+# Sans accent pour éviter les problèmes d'affichage sous windows
+
+  puts "Bienvenue sur FaitesLesComptes version #{FLCVERSION}
+logiciel open-source de comptabilite de tresorerie"
