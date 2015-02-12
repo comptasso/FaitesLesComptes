7fd2dbb4700d970de9fefb5d80ac2c0d7862fbf8
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2015-02-12 14:06:31 +0100

    patch à la place de put dans cash_control

--- spec/models/pdflc/flc_listing_spec.rb
+++ spec/models/pdflc/flc_listing_spec.rb
@@ -6,7 +6,7 @@ RSpec.configure do |c|
   # c.filter = {:wip=>true}
 end
 
-describe Pdflc::FlcBook do
+describe Pdflc::FlcListing do
   
   
   def render_file(pdf, file_name) 
@@ -42,22 +42,34 @@ describe Pdflc::FlcBook do
     Pdflc::FlcTable.any_instance.stub(:nb_pages).and_return 2
     Pdflc::FlcTable.any_instance.stub(:lines).and_return twenty_two_lines
     
-    Account.any_instance.stub(:period).and_return(double Period, 
+    Account.any_instance.stub(:period).and_return(@p = double(Period, 
       organism:double(Organism, title:'Asso Essai'),
-      long_exercice:'Exercice 2014')
+      long_exercice:'Exercice 2014'))
     Account.any_instance.stub(:cumulated_at).and_return 0
     
-    @b = Pdflc::FlcListing.new(from_account:@fas, 
-    from_date:Date.today.beginning_of_year,
-    to_date:Date.today.end_of_year,
-    fond:'Mise au point')
+    
   end
   
   # le pdf fera 8 pages, 2 pages pour chacun des comptes sauf pour le second
   # qui n'a pas de lignes.  
   it 'peut rendre le texte avec un fond' do
+    @b = Pdflc::FlcListing.new(from_account:@fas, 
+    from_date:Date.today.beginning_of_year,
+    to_date:Date.today.end_of_year,
+    fond:'Mise au point')
+    @b.draw_pdf
+  #  render_file(@b.pdf, 'listing') 
+  end
+  
+  it 'peut rendre le texte pour un exercice décalé' do
+    @p.stub(:long_exercice).and_return "Exercice \nsept. 2014 à août 2018"
+    @b = Pdflc::FlcListing.new(from_account:@fas, 
+    from_date:Date.today.beginning_of_year,
+    to_date:Date.today.end_of_year,
+    fond:'Mise au point')
     @b.draw_pdf
     render_file(@b.pdf, 'listing') 
+   
   end
   
 end
