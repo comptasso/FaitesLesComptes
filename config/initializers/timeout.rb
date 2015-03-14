781429d4c66d7d814744531854f26e391d4369eb
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2015-03-14 07:06:55 +0100

    suppression timeout pour les tests

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,4 +1,11 @@
-# lignes venant de la page heroku/rails-unicorn
+
 
 # config/initializers/timeout.rb
-Rack::Timeout.timeout = 15  # seconds
+# 
+# lignes venant de la page heroku/rails-unicorn
+# Rack::Timeout.timeout = 15  # seconds
+
+
+# Ligne ajoutée en mars 2015, suite à problèmes de test pour les features
+# utilisant javascript. Voir issue 55 sur Github/heroku/rack-timeout
+Rack::Timeout.timeout = (Rails.env.test? ? 0 : 20)
