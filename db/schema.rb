78f64f973a1dd0f71ccbd9c0bad8b36fc9b85f1b
jean-claude <jc.lepage@faiteslescomptes.fr>
2015-07-20 12:27:26 +0200

    premier commit de la branche noschema, modifs pour faire tourner le logiciel sur nouvelle configuration

--- db/schema.rb
+++ db/schema.rb
@@ -328,6 +328,13 @@ ActiveRecord::Schema.define(version: 20150718061649) do
     t.string   "postcode"
   end
 
+  create_table "organisms_users", id: false, force: true do |t|
+    t.integer "organism_id", null: false
+    t.integer "user_id",     null: false
+  end
+
+  add_index "organisms_users", ["organism_id", "user_id"], name: "index_organisms_users_on_organism_id_and_user_id", using: :btree
+
   create_table "periods", force: true do |t|
     t.date     "start_date"
     t.date     "close_date"
@@ -367,6 +374,16 @@ ActiveRecord::Schema.define(version: 20150718061649) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "sessions", force: true do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
+
   create_table "subscriptions", force: true do |t|
     t.integer  "day"
     t.integer  "mask_id"
@@ -383,12 +400,12 @@ ActiveRecord::Schema.define(version: 20150718061649) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                  default: "",         null: false
-    t.string   "encrypted_password",     default: "",         null: false
+    t.string   "email",                        default: "",         null: false
+    t.string   "encrypted_password",           default: "",         null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0
+    t.integer  "sign_in_count",                default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -396,7 +413,9 @@ ActiveRecord::Schema.define(version: 20150718061649) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "role",                   default: "standard"
+    t.string   "role",                         default: "standard"
+    t.boolean  "skip_confirm_change_password", default: false
+    t.integer  "tenant_id"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
