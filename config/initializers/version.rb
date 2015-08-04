bccb04595f359a2cf14f759356fb27dcf0fd0e0a
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2015-08-04 12:01:46 +0200

    suppression des tables lines et listings reapparues suite à une migration

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -100,4 +100,5 @@
 # FLCVERSION = '1.12.3RC' # Accès au module Compta même lorsque l'exercice est clos
 # FLCVERSION = '1.12.4RC' # Quand on édite un Transfert, on revient vers le bon mois
 # FLCVERSION = '1.12.5RC' # Les comptes de bilan prennent en compte le report de l'exercice précédent non clos
-FLCVERSION = '1.13.0RC' # Mise en place de piece_number pour les Writings
+# FLCVERSION = '1.13.0RC' # Mise en place de piece_number pour les Writings
+FLCVERSION = '1.13.1RC' # Suppression des tables lines et listings réapparues suite à une fusion 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150718061649) do
+ActiveRecord::Schema.define(version: 20150804094600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -256,28 +256,6 @@ ActiveRecord::Schema.define(version: 20150718061649) do
     t.string   "ref"
     t.integer  "writing_id"
     t.date     "writing_date"
-  end
-
-  create_table "lines", force: true do |t|
-    t.date     "line_date"
-    t.string   "narration"
-    t.integer  "nature_id"
-    t.integer  "destination_id"
-    t.decimal  "debit",          precision: 10, scale: 2
-    t.decimal  "credit",         precision: 10, scale: 2
-    t.integer  "listing_id"
-    t.boolean  "locked",                                  default: false
-    t.datetime "created_at",                                              null: false
-    t.datetime "updated_at",                                              null: false
-  end
-
-  create_table "listings", force: true do |t|
-    t.string   "title"
-    t.text     "description"
-    t.string   "image_url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "organism_id"
   end
 
   create_table "masks", force: true do |t|
