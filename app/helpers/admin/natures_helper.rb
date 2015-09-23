4538f2ac6d79996e2dab85f37ff858e53a0fdce5
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2015-09-23 07:45:54 +0200

    le formulaire admin/natures ne présente plus les secteur communs si CE

--- app/helpers/admin/natures_helper.rb
+++ app/helpers/admin/natures_helper.rb
@@ -9,17 +9,30 @@ module Admin::NaturesHelper
   #
   def comite_options_for_natures(period)
     @organism.sectors.collect do |s|
-      [OptionsForAssociationSelect.new(nat_group_title('Recettes', s),
-          :recettes_accounts, period,
-          s.id, {'data-type'=>'IncomeBook'}),
-        OptionsForAssociationSelect.new(nat_group_title('Dépenses', s),
-          :depenses_accounts, period,
-          s.id, {'data-type'=>'OutcomeBook'})
-      ]
+      nat_associations(period, s)
     end.flatten
   end
-
+  
+  def comite_options_for_natures_without_commun(period)
+    @organism.sectors.reject{|se| se.name == 'Commun'}.collect do |s|
+      nat_associations(period, s)
+    end.flatten
+  end
+  
+  
+  
+  
+  
   protected
+  
+  def nat_associations(period, sect)
+    [OptionsForAssociationSelect.new(nat_group_title('Recettes', sect),
+          :recettes_accounts, period,
+          sect.id, {'data-type'=>'IncomeBook'}),
+        OptionsForAssociationSelect.new(nat_group_title('Dépenses', sect),
+          :depenses_accounts, period,
+          sect.id, {'data-type'=>'OutcomeBook'})]
+  end
 
   def nat_group_title(titre, sector = nil)
     @organism.sectored? ? "#{titre} #{sector.name}" : titre
